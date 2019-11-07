8438a0d915197dd67b23047dfb6b098d0131d4f6
Albin Johansson <albinoscar99@hotmail.com>
2019-11-07 22:58:45 +0100

    Update doc

--- src/controller/wanderer_controller_impl.h
+++ src/controller/wanderer_controller_impl.h
@@ -4,6 +4,11 @@
 
 namespace wanderer::controller {
 
+/**
+ * The WandererControllerImpl class is an implementation of the IWandererController interface.
+ *
+ * @since 0.1.0
+ */
 class WandererControllerImpl final : public IWandererController {
  private:
   core::IWandererCore_uptr core = nullptr;
@@ -11,6 +16,12 @@ class WandererControllerImpl final : public IWandererController {
   WandererControllerImpl();
 
  public:
+  /**
+   * Creates and returns a unique pointer to an IWandererController instance.
+   *
+   * @return a unique pointer to an IWandererController instance.
+   * @since 0.1.0
+   */
   friend IWandererController_uptr CreateController();
 
   ~WandererControllerImpl() override;
--- src/core/wanderer_core_impl.h
+++ src/core/wanderer_core_impl.h
@@ -3,11 +3,22 @@
 
 namespace wanderer::core {
 
+/**
+ * The WandererCoreImpl class is an implementation of the IWandererCore interface.
+ *
+ * @since 0.1.0
+ */
 class WandererCoreImpl final : public IWandererCore {
  private:
   WandererCoreImpl();
 
  public:
+  /**
+   * Creates and returns a unique pointer to an IWandererCore instance.
+   *
+   * @return a unique pointer to an IWandererCore instance.
+   * @since 0.1.0
+   */
   friend IWandererCore_uptr CreateCore();
 
   ~WandererCoreImpl() override;
