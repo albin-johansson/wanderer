606b37ff325ec5c8974df7e759fa097001e1e9c2
Albin Johansson <albinoscar99@hotmail.com>
2019-11-09 22:30:41 +0100

    Document Direction

--- src/core/direction.h
+++ src/core/direction.h
@@ -2,6 +2,11 @@
 
 namespace wanderer::core {
 
+/**
+ * The Direction enum provides values for 4 different directions: up, right, down and left.
+ *
+ * @since 0.1.0
+ */
 enum class Direction { UP, RIGHT, DOWN, LEFT };
 
 }
