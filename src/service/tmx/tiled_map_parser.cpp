1060de06f8076a4d1e0c471c0415d2e66d516a91
Albin Johansson <albinoscar99@hotmail.com>
2019-11-28 19:24:58 +0100

    Remove using namespace std in TileMapParser

--- src/service/tmx/tiled_map_parser.cpp
+++ src/service/tmx/tiled_map_parser.cpp
@@ -2,10 +2,7 @@
 #include "pugixml.hpp"
 #include "sprite_sheet.h"
 #include "bad_state_exception.h"
-#include <SDL.h>
 #include <sstream>
-
-using namespace std;
 
 namespace albinjohansson::wanderer {
 
@@ -17,19 +14,20 @@ pugi::xml_document TiledMapParser::LoadDocument(const std::string& path) {
   pugi::xml_document doc;
   pugi::xml_parse_result result = doc.load_file(path.c_str());
   if (!result) {
-    throw BadStateException("Failed to load: " + path + ", Error:" + string(result.description()));
+    throw BadStateException("Failed to load: " + path + ", Error:"
+                                + std::string(result.description()));
   }
   return doc;
 }
 
-vector<TTS> TiledMapParser::CreateTileSetInfo(ImageGenerator& imageGenerator,
-                                              const pugi::xml_node& mapRootNode) {
-  vector<TTS> tmps;
+std::vector<TTS> TiledMapParser::CreateTileSetInfo(ImageGenerator& imageGenerator,
+                                                   const pugi::xml_node& mapRootNode) {
+  std::vector<TTS> tmps;
   tmps.reserve(20);
 
   for (auto ts : mapRootNode.children("tileset")) {
     auto firstgid = static_cast<uint16_t>(ts.attribute("firstgid").as_int());
-    auto source = string(ts.attribute("source").as_string());
+    auto source = std::string(ts.attribute("source").as_string());
 
     auto tsDocument = LoadDocument("resources/map/world/" + source); // FIXME
     auto tsRootNode = tsDocument.child("tileset");
@@ -39,7 +37,7 @@ vector<TTS> TiledMapParser::CreateTileSetInfo(ImageGenerator& imageGenerator,
     auto tilecount = tsRootNode.attribute("tilecount").as_int();
 
     auto imageNode = tsRootNode.child("image");
-    auto imgSource = string(imageNode.attribute("source").as_string()); // Relative to map
+    auto imgSource = std::string(imageNode.attribute("source").as_string()); // Relative to map
 //    auto imgWidth = imageNode.attribute("width").as_int();
 //    auto imgHeight = imageNode.attribute("height").as_int();
 
@@ -101,7 +99,8 @@ void TiledMapParser::PrepareTileSets(const std::vector<TTS>& tempTileSets,
   }
 }
 
-unique_ptr<TileMap> TiledMapParser::LoadMap(ImageGenerator& imageGenerator, const string& file) {
+std::unique_ptr<TileMap> TiledMapParser::LoadMap(ImageGenerator& imageGenerator,
+                                                 const std::string& file) {
   auto mapDocument = LoadDocument(file);
   auto mapRootNode = mapDocument.child("map");
 
