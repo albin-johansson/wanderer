28d4844a065a0bd96460ed1d30d496d60521c778
Albin Johansson <albinoscar99@hotmail.com>
2019-12-02 08:33:17 +0100

    Update entity_draw_delegate.cpp

--- src/core/entities/entitystate/entity_draw_delegate.cpp
+++ src/core/entities/entitystate/entity_draw_delegate.cpp
@@ -11,6 +11,7 @@ void EntityDrawDelegate::Draw(Renderer& renderer, const Viewport& viewport,
   auto dst = Rectangle(x, y, entity.GetWidth(), entity.GetHeight());
   renderer.RenderTexture(entity.GetSpriteSheet(), src, dst);
 
+  // TODO remove
   auto hbWidth = (static_cast<float>(entity.GetHealth()) / 100.0f) * 120.0f;
   auto hbX = (x + (entity.GetWidth() / 2.0f)) - (hbWidth / 2);
   renderer.SetColor(0xff, 0, 0);
