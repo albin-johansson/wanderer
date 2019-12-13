83be518cecbcb6c79883e89d23d9ba3dc8c355e2
Albin Johansson <albinoscar99@hotmail.com>
2019-12-13 12:17:28 +0100

    Update bad_state_exception.h

--- src/core/bad_state_exception.h
+++ src/core/bad_state_exception.h
@@ -6,24 +6,35 @@ namespace albinjohansson::wanderer {
 
 /**
  * The BadStateException class is a subclass of std::exception that represents an exception caused
- * by bad state.
+ * by some kind of bad state.
  *
  * @since 0.1.0
  */
 class BadStateException final : public std::exception {
  private:
-  const char* message;
+  const char* message = nullptr;
 
  public:
+  /**
+   * @since 0.1.0
+   */
   BadStateException();
 
+  /**
+   * @param what the error message.
+   * @since 0.1.0
+   */
   explicit BadStateException(const std::string& what);
 
+  /**
+   * @param what the error message.
+   * @since 0.1.0
+   */
   explicit BadStateException(const char* what);
 
   ~BadStateException() override;
 
-  const char* what() const noexcept override;
+  [[nodiscard]] const char* what() const noexcept override;
 };
 
 }
