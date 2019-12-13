cda7d9960f2dfc59dfeb7994478048a8583527e2
Albin Johansson <albinoscar99@hotmail.com>
2019-12-13 12:34:31 +0100

    Update player.h

--- src/core/entities/player.h
+++ src/core/entities/player.h
@@ -5,6 +5,12 @@ namespace albinjohansson::wanderer {
 
 class Input;
 
+/**
+ * The IPlayer interface specifies the player in the game.
+ *
+ * @see IEntity
+ * @since 0.1.0
+ */
 class IPlayer : public virtual IEntity {
  protected:
   IPlayer() = default;
@@ -12,6 +18,13 @@ class IPlayer : public virtual IEntity {
  public:
   ~IPlayer() override = default;
 
+  /**
+   * Handles the supplied input.
+   *
+   * @param input a reference to the input state object.
+   * @param core a reference to the associated core instance.
+   * @since 0.1.0
+   */
   virtual void HandleInput(const Input& input, const IWandererCore& core) = 0;
 };
 
