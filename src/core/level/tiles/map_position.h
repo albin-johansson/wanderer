ae96af56eb6dff3d1786446334075ce61379f5e8
Albin Johansson <albinoscar99@hotmail.com>
2019-12-13 15:01:53 +0100

    Update map_position.h

--- src/core/level/tiles/map_position.h
+++ src/core/level/tiles/map_position.h
@@ -2,6 +2,11 @@
 
 namespace albinjohansson::wanderer {
 
+/**
+ * The MapPosition struct represents a map position in the format (row, col).
+ *
+ * @since 0.1.0
+ */
 struct MapPosition final {
   int row = 0;
   int col = 0;
