51a4ee3a2925ac928a1e132312b0f01f3df94092
Albin Johansson <albinoscar99@hotmail.com>
2019-12-13 12:21:10 +0100

    Update render_depth.h

--- src/core/render_depth.h
+++ src/core/render_depth.h
@@ -2,9 +2,31 @@
 
 namespace albinjohansson::wanderer {
 
+/**
+ * The RenderDepth struct contains constants for the possible render depths.
+ *
+ * @since 0.1.0
+ */
 struct RenderDepth final {
+  /**
+   * The least possible render depth value.
+   *
+   * @since 0.1.0
+   */
   static constexpr int MIN = 0;
+
+  /**
+   * The highest possible render depth value.
+   *
+   * @since 0.1.0
+   */
   static constexpr int MAX = 9;
+
+  /**
+   * The amount of possible render depth values.
+   *
+   * @since 0.1.0
+   */
   static constexpr int RANGE = MAX + 1;
 
   RenderDepth() = delete;
