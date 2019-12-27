964f0d5e2c1d6b01791d2f738513e8c8b1f6bca2
Albin Johansson <albinoscar99@hotmail.com>
2019-12-27 15:51:41 +0100

    MapPosition is now hashable

--- src/core/level/tiles/map_position.h
+++ src/core/level/tiles/map_position.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <functional>
 
 namespace albinjohansson::wanderer {
 
@@ -10,6 +11,21 @@ namespace albinjohansson::wanderer {
 struct MapPosition final {
   int row = 0;
   int col = 0;
+
+  [[nodiscard]] bool operator==(const MapPosition& other) const noexcept {
+    return row == other.row && col == other.col;
+  }
 };
 
 }
+
+template<>
+struct std::hash<albinjohansson::wanderer::MapPosition> {
+  std::size_t operator()(const albinjohansson::wanderer::MapPosition& k) const {
+    // http://stackoverflow.com/a/1646913/126995
+    std::size_t res = 17;
+    res = res * 31 + std::hash<int>()(k.row);
+    res = res * 31 + std::hash<int>()(k.col);
+    return res;
+  }
+};
