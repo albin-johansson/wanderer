f1ae785ec96f42166e4c12f8d0fb8fa802f5c142
Albin Johansson <albinoscar99@hotmail.com>
2020-01-27 23:43:09 +0100

    Improve random utils

--- src/core/math/random_utils.h
+++ src/core/math/random_utils.h
@@ -1,9 +1,4 @@
 #pragma once
-#include <cstdint>
-#include <random>
-#include <type_traits>
-#include <limits>
-#include "timer.h"
 
 namespace albinjohansson::wanderer {
 
@@ -19,39 +14,15 @@ class RandomUtils final {
   ~RandomUtils() = default;
 
   /**
-   * Returns a random floating-point or integral value.
-   *
-   * @tparam T the type of returned value, int by default. Must be an integral or floating-point
-   * type.
-   * @return a random floating-point or integral value.
-   * @since 0.1.0
-   */
-  template<typename T = int>
-  [[nodiscard]] static T get_rand() noexcept {
-    static_assert(std::is_floating_point_v<T> || std::is_integral_v<T>);
-
-    std::ranlux24_base rnd(centurion::Timer::high_res());
-    return static_cast<T>(rnd());
-  }
-
-  /**
    * Returns a random integer in the specified interval. The generated value will be in the range
    * [min, max].
    *
    * @param min the smallest possible value.
    * @param max the largest possible value.
-   * @tparam the type of the random value, int by default. Must be an integral or floating-point
-   * type.
    * @return a random integer in the specified interval.
    * @since 0.1.0
    */
-  template<typename T = int>
-  [[nodiscard]] static int get_int(T min, T max) noexcept {
-    static_assert(std::is_floating_point_v<T> || std::is_integral_v<T>);
-
-    std::ranlux24_base rnd(centurion::Timer::high_res());
-    return static_cast<T>((rnd() % (max + 1 - min)) + min);
-  }
+  [[nodiscard]] static int get_int(int min, int max) noexcept;
 
   /**
    * Returns a random bool value.
--- test/unittests/random_utils_test.cpp
+++ test/unittests/random_utils_test.cpp
@@ -1,15 +1,13 @@
 #include "catch.hpp"
 #include "random_utils.h"
 #include <string>
-#include <array>
 
 using namespace albinjohansson::wanderer;
 
 TEST_CASE("RandomUtils::get_int", "[RandomUtils]") {
   SECTION("Test that specifying the same min and max value returns the same value.") {
     for (auto i = 0; i < 10; ++i) {
-      const auto x = RandomUtils::get_rand();
-      CHECK(x == RandomUtils::get_int(x, x));
+      CHECK(i == RandomUtils::get_int(i, i));
     }
   }
   SECTION("Interval test") {
@@ -21,37 +19,4 @@ TEST_CASE("RandomUtils::get_int", "[RandomUtils]") {
       CHECK(x <= max);
     }
   }
-}
-
-TEST_CASE("RandomUtils::get_rand", "[RandomUtils]") {
-  CHECK_NOTHROW(RandomUtils::get_rand<short>());
-  CHECK_NOTHROW(RandomUtils::get_rand<int>());
-  CHECK_NOTHROW(RandomUtils::get_rand<long>());
-  CHECK_NOTHROW(RandomUtils::get_rand<unsigned>());
-  CHECK_NOTHROW(RandomUtils::get_rand<float>());
-  CHECK_NOTHROW(RandomUtils::get_rand<double>());
-}
-
-TEST_CASE("RandomUtils::get_bool", "[RandomUtils]") {
-  std::array<bool, 1'000> values{};
-  for (auto& value : values) {
-    value = RandomUtils::get_bool();
-  }
-
-  auto false_count = 0;
-  auto true_count = 0;
-
-  for (const auto value : values) {
-    if (value) {
-      ++true_count;
-    } else {
-      ++false_count;
-    }
-  }
-
-  const auto half = values.size() / 2.0;
-  const auto tenth = values.size() / 2.0;
-
-  CHECK(std::abs(true_count - half) < tenth);
-  CHECK(std::abs(false_count - half) < tenth);
 }
