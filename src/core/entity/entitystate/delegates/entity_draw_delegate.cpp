0abd1590bb23daec2c524e01fbca2b6612cfc57c
Albin Johansson <albinoscar99@hotmail.com>
2020-05-25 21:30:42 +0200

    Update entity_draw_delegate.cpp

--- src/core/entity/entitystate/delegates/entity_draw_delegate.cpp
+++ src/core/entity/entitystate/delegates/entity_draw_delegate.cpp
@@ -4,6 +4,7 @@
 #include <renderer.h>
 
 #include "entity.h"
+#include "hitbox.h"
 #include "viewport.h"
 
 using namespace centurion;
@@ -21,6 +22,18 @@ void EntityDrawDelegate::draw(Renderer& renderer,
   const auto src = IRect{srcX, srcY, 64, 64};
   const auto dst = FRect{x, y, entity.get_width(), entity.get_height()};
   renderer.render_tf(entity.get_sprite_sheet(), src, dst);
+
+#ifdef WANDERER_DEBUG
+  // TODO CTN 4.1
+  const auto& hb = entity.get_hitbox().get_bounds();
+  const auto hx = hb.x() - renderer.translation_viewport().x();
+  const auto hy = hb.y() - renderer.translation_viewport().y();
+
+  const FRect box = {hx, hy, hb.width(), hb.height()};
+
+  renderer.set_color(color::red);
+  renderer.draw_rect_f(box);
+#endif  // WANDERER_DEBUG
 }
 
 }  // namespace wanderer
