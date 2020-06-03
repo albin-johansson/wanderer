571005cf090321180a614b6bf6ff37368de38e20
Albin Johansson <albinoscar99@hotmail.com>
2020-06-03 23:36:45 +0200

    Update update_movement.cpp

--- src/system/update_movement.cpp
+++ src/system/update_movement.cpp
@@ -25,16 +25,6 @@ void update_direction(Movable& movable) noexcept
 
 void update_movement(entt::registry& registry, float delta)
 {
-//  registry.view<Movable>().each([&](Movable& movable) noexcept {
-//    movable.oldPos = movable.currentPos;
-//    // TODO update hitbox
-//    //    movable.hitbox.set_x(m_currPosition.x);
-//    //    movable.hitbox.set_y(m_currPosition.y);
-//    update_direction(movable);
-//    movable.currentPos.add(movable.velocity.x * delta,
-//                           movable.velocity.y * delta);
-//  });
-
   const auto entities = registry.view<Movable>();
   for (const auto entity : entities) {
     auto& movable = entities.get<Movable>(entity);
