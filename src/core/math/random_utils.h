a0a1c09a7cd99efa1e8da982fb4042cf5f6618a5
Albin Johansson <albinoscar99@hotmail.com>
2020-06-23 15:17:54 +0200

    Update random_utils.h

--- src/core/math/random_utils.h
+++ src/core/math/random_utils.h
@@ -1,8 +1,48 @@
 #pragma once
+
+#ifndef NDEBUG
+#include <log.h>
+#endif  // NDEBUG
+
+#include <timer.h>
+
 #include <random>
 
 namespace wanderer::math {
+namespace {
 
-[[nodiscard]] int random_int(int min, int max);
+inline std::mt19937 random;
+
+}
+
+void init_rnd() noexcept
+{
+  const auto seed = centurion::Timer::high_res();
+#ifndef NDEBUG
+  centurion::Log::info("Using seed: %u (for RNG)", seed);
+#endif  // NDEBUG
+  random.seed(seed);
+}
+
+/**
+ * Returns a random value in the specified inclusive range.
+ *
+ * @tparam T the type of the value to be obtained, must be either integral or
+ * floating-point.
+ * @param min the minimum possible value.
+ * @param max the maximum possible value.
+ * @return a random integer in the specified range.
+ */
+template <typename T>
+[[nodiscard]] T get_random(T min, T max) noexcept
+{
+  if constexpr (std::is_floating_point_v<T>) {
+    std::uniform_real_distribution<T> dist{min, max};
+    return dist(random);
+  } else {
+    std::uniform_int_distribution<T> dist{min, max};
+    return dist(random);
+  }
+}
 
 }  // namespace wanderer::math
