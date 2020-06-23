79b0dd66d6f290ce9aa947b0e6359354b5479a46
Albin Johansson <albinoscar99@hotmail.com>
2020-06-24 00:43:24 +0200

    Update viewport_system.h

--- src/system/viewport_system.h
+++ src/system/viewport_system.h
@@ -1,15 +1,24 @@
 #pragma once
 
-#include <area.h>
-
 #include "viewport.h"
 #include "wanderer_stdinc.h"
 
 namespace wanderer::system::viewport {
 
+/**
+ * Updates the position of the viewport by slowly tracking a movable entity.
+ *
+ * @pre <code>viewportEntity</code> must have a <code>Viewport</code> component.
+ * @pre <code>playerEntity</code> must have a <code>Movable</code> component.
+ *
+ * @param registry the registry that holds the viewport and the player.
+ * @param viewportEntity the entity ID for the viewport.
+ * @param movableEntity the entity ID for the movable.
+ * @param delta the delta time, in seconds.
+ */
 void update(entt::registry& registry,
             ViewportEntity viewportEntity,
-            entt::entity playerEntity,
+            entt::entity movableEntity,
             Delta delta);
 
 }  // namespace wanderer::system::viewport
