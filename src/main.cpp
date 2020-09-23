d067ab58636096e9df3aa4cd94eb0dc953c2e805
Albin Johansson <albinoscar99@hotmail.com>
2020-09-23 19:40:10 +0200

    Improve random number generation

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,20 @@
 #endif
 
 #include <centurion.hpp>
-#include <font.hpp>
 #include <log.hpp>
-#include <surface.hpp>
 
 #include "application.hpp"
-#include "random_utils.hpp"
 
 using namespace wanderer;
 
 int main(int, char**)
 {
-  math::init_rnd();
   cen::library lib;
 
   cen::log::set_priority(cen::log::priority::info);
 
   application app;
   app.run();
+
   return 0;
 }
