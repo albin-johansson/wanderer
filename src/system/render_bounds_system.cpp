6f26f81fd6b8e13110a43f142a3f92510887dd5a
Albin Johansson <albinoscar99@hotmail.com>
2020-09-24 14:01:32 +0200

    Update render_bounds_system.cpp

--- src/system/render_bounds_system.cpp
+++ src/system/render_bounds_system.cpp
@@ -1,9 +1,53 @@
 #include "render_bounds_system.hpp"
 
-#include "floating.hpp"
 #include "game_constants.hpp"
 
 namespace wanderer::sys {
+namespace {
+
+[[nodiscard]] auto calculate_min_col(float viewportX) noexcept -> int
+{
+  const auto minCol = static_cast<int>(viewportX / g_tileSize<float>);
+  if (minCol < 0) {
+    return 0;
+  } else {
+    return minCol;
+  }
+}
+
+[[nodiscard]] auto calculate_min_row(float viewportY) noexcept -> int
+{
+  const auto minRow = static_cast<int>(viewportY / g_tileSize<float>);
+  if (minRow < 0) {
+    return 0;
+  } else {
+    return minRow;
+  }
+}
+
+[[nodiscard]] auto calculate_max_col(float viewportMaxX, int numCols) noexcept
+    -> int
+{
+  const auto maxCol = static_cast<int>(viewportMaxX / g_tileSize<float>) + 1;
+  if (maxCol > numCols) {
+    return numCols;
+  } else {
+    return maxCol;
+  }
+}
+
+[[nodiscard]] auto calculate_max_row(float viewportMaxY, int numRows) noexcept
+    -> int
+{
+  const auto maxRow = static_cast<int>(viewportMaxY / g_tileSize<float>) + 1;
+  if (maxRow > numRows) {
+    return numRows;
+  } else {
+    return maxRow;
+  }
+}
+
+}  // namespace
 
 auto calculate_render_bounds(entt::registry& registry,
                              const comp::viewport::entity viewportEntity,
@@ -14,26 +58,10 @@ auto calculate_render_bounds(entt::registry& registry,
   const auto& viewportBounds = viewport.bounds;
 
   comp::render_bounds bounds;
-  bounds.minCol = round(viewportBounds.x()) / g_tileSize<int>;
-  bounds.minRow = round(viewportBounds.y()) / g_tileSize<int>;
-  bounds.maxCol = (round(viewportBounds.max_x()) / g_tileSize<int>)+1;
-  bounds.maxRow = (round(viewportBounds.max_y()) / g_tileSize<int>)+1;
-
-  if (bounds.minCol < 0) {
-    bounds.minCol = 0;
-  }
-
-  if (bounds.minRow < 0) {
-    bounds.minRow = 0;
-  }
-
-  if (bounds.maxRow > rows) {
-    bounds.maxRow = rows;
-  }
-
-  if (bounds.maxCol > cols) {
-    bounds.maxCol = cols;
-  }
+  bounds.minCol = calculate_min_col(viewportBounds.x());
+  bounds.minRow = calculate_min_row(viewportBounds.y());
+  bounds.maxCol = calculate_max_col(viewportBounds.max_x(), cols);
+  bounds.maxRow = calculate_max_row(viewportBounds.max_y(), rows);
 
   return bounds;
 }
