f8c6ed77ddb6d1d34692e1933ed749626ac32999
Albin Johansson <albinoscar99@hotmail.com>
2020-09-24 10:33:44 +0200

    Update image_cache.hpp

--- src/core/resource/image_cache.hpp
+++ src/core/resource/image_cache.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
+#include <entt.hpp>
 #include <texture.hpp>
-
-#include "wanderer_stdinc.hpp"
 
 namespace wanderer {
 
 /**
  * @typedef image_cache
+ *
  * @brief Used for caches that store centurion textures.
  */
 using image_cache = entt::cache<cen::texture>;
