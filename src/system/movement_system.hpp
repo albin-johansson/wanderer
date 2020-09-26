23b8766bdd25fe30f0a4308ecb65ec57ed9e002d
Albin Johansson <albinoscar99@hotmail.com>
2020-09-26 13:40:24 +0200

    Update movement_system.hpp

--- src/system/movement_system.hpp
+++ src/system/movement_system.hpp
@@ -8,6 +8,18 @@
 
 namespace wanderer::sys {
 
+/**
+ * @brief Updates the state of components related to movement.
+ *
+ * @details This system iterates entities with `movable` components, whilst
+ * also checking if they feature `hitbox` and/or `depth_drawable` components.
+ * This system will perform collision detection for movables with hitboxes.
+ * Furthermore, `depth_drawable` components will have their coordinates
+ * updated by this system.
+ *
+ * @param level the currently active level.
+ * @param dt the delta time.
+ */
 void update_movement(level& level, delta dt);
 
 }  // namespace wanderer::sys
