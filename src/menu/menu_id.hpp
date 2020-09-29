f1d0195e4c05f9814879dd464f4cbb4f94d8226a
Albin Johansson <albinoscar99@hotmail.com>
2020-09-29 23:24:58 +0200

    Create menu_id.hpp

---
+++ src/menu/menu_id.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace wanderer {
+
+enum class menu_id
+{
+  in_game,
+  home,
+  settings,
+  controls
+};
+
+NLOHMANN_JSON_SERIALIZE_ENUM(menu_id,
+                             {{menu_id::in_game, "in_game"},
+                              {menu_id::home, "home"},
+                              {menu_id::settings, "settings"},
+                              {menu_id::controls, "controls"}});
+
+}  // namespace wanderer
