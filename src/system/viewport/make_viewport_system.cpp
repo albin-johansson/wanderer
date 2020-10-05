116eb04ad2803b619c6cb22744bb665100fb2750
Albin Johansson <albinoscar99@hotmail.com>
2020-10-05 13:11:05 +0200

    Update make_viewport_system.cpp

--- src/system/viewport/make_viewport_system.cpp
+++ src/system/viewport/make_viewport_system.cpp
@@ -9,7 +9,7 @@ auto make_viewport(entt::registry& registry) -> comp::viewport::entity
   const auto entity = registry.create();
 
   auto& viewport = registry.emplace<comp::viewport>(entity);
-  viewport.bounds = {{0, 0}, {g_logicalWidth<float>, g_logicalHeight<float>}};
+  viewport.bounds = {{}, {g_logicalWidth<float>, g_logicalHeight<float>}};
   viewport.levelSize.width = 1000;
   viewport.levelSize.height = 1000;  // FIXME update automatically
 
