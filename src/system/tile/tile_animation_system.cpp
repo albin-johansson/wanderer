8012bfe5a68de30e2f0c9b8a7f047352fda9a9ed
Albin Johansson <albinoscar99@hotmail.com>
2020-10-05 13:06:55 +0200

    Update tile_animation_system.cpp

--- src/system/tile/tile_animation_system.cpp
+++ src/system/tile/tile_animation_system.cpp
@@ -8,20 +8,18 @@ namespace wanderer::sys::tile {
 
 void update_animation(entt::registry& registry)
 {
-  const auto entities = registry.view<comp::animated_tile>();
-  for (const auto entity : entities) {
-    auto& animatedTile = entities.get<comp::animated_tile>(entity);
-
-    const auto now = cen::counter::ticks();
-    const auto elapsed = now - animatedTile.then;
-    if (elapsed >= animatedTile.frames.at(animatedTile.index).duration) {
-      animatedTile.then = now;
-      ++animatedTile.index;
-      if (animatedTile.index >= animatedTile.frames.size()) {
-        animatedTile.index = 0;
-      }
-    }
-  }
+  registry.view<comp::animated_tile>().each(
+      [](comp::animated_tile& animatedTile) {
+        const auto now = cen::counter::ticks();
+        const auto elapsed = now - animatedTile.then;
+        if (elapsed >= animatedTile.frames.at(animatedTile.index).duration) {
+          animatedTile.then = now;
+          ++animatedTile.index;
+          if (animatedTile.index >= animatedTile.frames.size()) {
+            animatedTile.index = 0;
+          }
+        }
+      });
 }
 
 }  // namespace wanderer::sys::tile
