e1c7ec7e2df0f56f61b6ebc8d5d8e85788c7c14e
Albin Johansson <albinoscar99@hotmail.com>
2020-10-05 13:05:11 +0200

    Update layer_rendering_system.cpp

--- src/system/layer/layer_rendering_system.cpp
+++ src/system/layer/layer_rendering_system.cpp
@@ -16,7 +16,7 @@ void render(entt::registry& registry,
       const auto r = static_cast<std::size_t>(row);
       const auto c = static_cast<std::size_t>(col);
 
-      const tile_id id = layer.matrix.at(r).at(c);
+      const auto id = layer.matrix.at(r).at(c);
       if (id == g_emptyTile) {
         continue;
       }
