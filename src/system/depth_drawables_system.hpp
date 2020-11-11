ee970459042d51b0511449794f8627cdacfaa264
Albin Johansson <albinoscar99@hotmail.com>
2020-11-11 13:23:17 +0100

    Update depth_drawables_system.hpp

--- src/system/depth_drawables_system.hpp
+++ src/system/depth_drawables_system.hpp
@@ -5,8 +5,27 @@
 
 namespace wanderer::sys::depthdrawable {
 
+/**
+ * \brief Sorts all depth drawables according to their depth and center
+ * y-coordinate.
+ *
+ * \param registry the registry that holds the depth drawables.
+ *
+ * \since 0.1.0
+ */
 void sort(entt::registry& registry);
 
+/**
+ * \brief Renders all depth drawables.
+ *
+ * \note Make sure to call `sort(entt::registry&)` before invoking this
+ * function.
+ *
+ * \param registry the registry that holds the depth drawables.
+ * \param renderer the renderer that will be used.
+ *
+ * \since 0.1.0
+ */
 void render(const entt::registry& registry, cen::renderer& renderer);
 
 }  // namespace wanderer::sys::depthdrawable
