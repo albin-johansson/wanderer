343d3a197275ee65b1fa9688c1c2262eba8f9788
Albin Johansson <albinoscar99@hotmail.com>
2020-11-11 19:11:52 +0100

    Update make_renderer.cpp

--- src/core/make_renderer.cpp
+++ src/core/make_renderer.cpp
@@ -10,12 +10,12 @@ auto make_renderer(const cen::window& window) -> cen::renderer
 {
   cen::renderer renderer{window};
 
-  const auto typewriter = "resource/font/type_writer.ttf";
+  const auto* typewriter = "resource/font/type_writer.ttf";
   renderer.emplace_font("typewriter_s"_hs, typewriter, 8);
   renderer.emplace_font("typewriter_m"_hs, typewriter, 16);
   renderer.emplace_font("typewriter_l"_hs, typewriter, 24);
 
-  renderer.set_logical_size({g_logicalWidth<>, g_logicalHeight<>});
+  renderer.set_logical_size({g_logicalWidth<int>, g_logicalHeight<int>});
   renderer.set_logical_integer_scale(true);
 
   return renderer;
