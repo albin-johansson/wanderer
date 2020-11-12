f78a70e5eaf117dd37cd1a68932039ecb8030e1f
Albin Johansson <albinoscar99@hotmail.com>
2020-11-12 19:29:30 +0100

    Update depth.hpp

--- src/core/aliases/depth.hpp
+++ src/core/aliases/depth.hpp
@@ -6,4 +6,10 @@ namespace wanderer {
 
 using depth = fluent::NamedType<int, struct depth_tag_t, fluent::Comparable>;
 
+[[nodiscard]] constexpr auto operator"" _depth(unsigned long long d) noexcept
+    -> depth
+{
+  return depth{static_cast<int>(d)};
 }
+
+}  // namespace wanderer
