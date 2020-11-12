5f62aab3766ed0bf2eee2aab1022e1518eb5790d
Albin Johansson <albinoscar99@hotmail.com>
2020-11-12 21:03:54 +0100

    Update layer_rendering_system.cpp

--- src/system/layer/layer_rendering_system.cpp
+++ src/system/layer/layer_rendering_system.cpp
@@ -1,6 +1,7 @@
 #include "layer_rendering_system.hpp"
 
 #include "component/animated_tile.hpp"
+#include "layer_system.hpp"
 #include "tile_rendering_system.hpp"
 
 namespace wanderer::sys::layer {
@@ -11,25 +12,16 @@ void render(const entt::registry& registry,
             const comp::tileset& tileset,
             const comp::render_bounds& bounds)
 {
-  for (auto row = bounds.minRow; row < bounds.maxRow; ++row) {
-    for (auto col = bounds.minCol; col < bounds.maxCol; ++col) {
-      const auto id = layer.matrix.at(static_cast<std::size_t>(row))
-                          .at(static_cast<std::size_t>(col));
-      if (id == g_emptyTile) {
-        continue;
-      }
-
-      const auto entity = tileset.tiles.at(id);
-
-      if (registry.has<comp::animated_tile>(entity.get())) {
-        const auto& tile = tile::animated_tile(registry, entity, tileset);
-        tile::render(renderer, tile, row, col);
-      } else {
-        const auto& tile = registry.get<comp::tile>(entity.get());
-        tile::render(renderer, tile, row, col);
-      }
+  visit(layer, bounds, [&](const tile_id id, const int row, const int col) {
+    const auto entity = tileset.tiles.at(id);
+    if (registry.has<comp::animated_tile>(entity.get())) {
+      const auto& tile = tile::animated_tile(registry, entity, tileset);
+      tile::render(renderer, tile, row, col);
+    } else {
+      const auto& tile = registry.get<comp::tile>(entity.get());
+      tile::render(renderer, tile, row, col);
     }
-  }
+  });
 }
 
 void render_ground(const entt::registry& registry,
