742b8fa93b8ffff55ac0267c273509ae72dbfb06
Albin Johansson <albinoscar99@hotmail.com>
2020-11-18 16:46:28 +0100

    Update animation_system.hpp

--- src/system/animation_system.hpp
+++ src/system/animation_system.hpp
@@ -4,6 +4,13 @@
 
 namespace wanderer::sys::animated {
 
+/**
+ * \brief Updates all `animated` components in a registry.
+ *
+ * \param registry the registry that contains the animated components.
+ *
+ * \since 0.1.0
+ */
 void update(entt::registry& registry) noexcept;
 
 }  // namespace wanderer::sys::animated
