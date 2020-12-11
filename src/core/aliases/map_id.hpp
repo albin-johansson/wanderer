12b7252c258ebc50a18198c825b03dd3d9e4e034
Albin Johansson <albinoscar99@hotmail.com>
2020-12-11 17:01:59 +0100

    Add map_id alias

---
+++ src/core/aliases/map_id.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "named_type.hpp"
+
+namespace wanderer {
+namespace detail {
+struct map_id_t;
+}
+
+using map_id = fluent::NamedType<int,
+                                 detail::map_id_t,
+                                 fluent::Comparable,
+                                 fluent::Hashable,
+                                 fluent::PreIncrementable>;
+
+}  // namespace wanderer
