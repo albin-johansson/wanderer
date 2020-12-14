fd4511f629b62364389932003462a7d5e1161e08
Albin Johansson <albinoscar99@hotmail.com>
2020-12-14 23:56:29 +0100

    Update portal.hpp

--- src/component/portal.hpp
+++ src/component/portal.hpp
@@ -1,22 +1,27 @@
 #pragma once
 
-#include <entt.hpp>
 #include <filesystem>  // path
 
+#include "entity_type.hpp"
 #include "map_id.hpp"
 #include "maybe.hpp"
+#include "null_entity.hpp"
 
 namespace wanderer::comp {
+namespace detail {
+struct portal_entity_t;
+}
 
 struct portal final
 {
+  using entity = entity_type<detail::portal_entity_t>;
   std::filesystem::path path;  ///< Path to target map file.
   maybe<map_id> target;        ///< Identifier associated with target map.
 };
 
 struct is_within_portal final
 {
-  entt::entity portal{entt::null};  ///< The entity ID of the associated portal.
+  portal::entity portalEntity{null<portal>()};  ///< The associated portal.
 };
 
 }  // namespace wanderer::comp
