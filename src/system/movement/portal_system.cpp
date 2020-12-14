9ac30efc3a3e36f099624de5230d3adceb1edba9
Albin Johansson <albinoscar99@hotmail.com>
2020-12-14 09:48:04 +0100

    Update portal_system.cpp

--- src/system/movement/portal_system.cpp
+++ src/system/movement/portal_system.cpp
@@ -8,22 +8,25 @@ namespace wanderer::sys::portal {
 
 void update(entt::registry& registry, const comp::player::entity player)
 {
-  const auto& hitbox = registry.get<comp::hitbox>(player);
-  registry.view<comp::portal, comp::hitbox>().each(
-      [&](const entt::entity e, const comp::portal& portal, comp::hitbox& hb) {
-        hb.enabled = true;
+  const auto& playerHitbox = registry.get<comp::hitbox>(player);
+  const auto view = registry.view<comp::portal, comp::hitbox>();
+  view.each([&](const entt::entity e,
+                const comp::portal& portal,
+                comp::hitbox& hitbox) {
+    hitbox.enabled = true;
 
-        if (sys::hitbox::intersects(hitbox, hb)) {
-          registry.emplace_or_replace<comp::is_within_portal>(player, e);
-        } else if (const auto* iwp =
-                       registry.try_get<comp::is_within_portal>(player)) {
-          if (iwp->portal == e) {
-            registry.remove<comp::is_within_portal>(player);
-          }
-        }
+    if (sys::hitbox::intersects(playerHitbox, hitbox)) {
+      registry.emplace_or_replace<comp::is_within_portal>(player, e);
 
-        hb.enabled = false;
-      });
+    } else if (const auto* iwp =
+                   registry.try_get<comp::is_within_portal>(player)) {
+      if (iwp->portal == e) {
+        registry.remove<comp::is_within_portal>(player);
+      }
+    }
+
+    hitbox.enabled = false;
+  });
 }
 
 }  // namespace wanderer::sys::portal
