5764680b86fc4f2f3deb008b38fa816c23069302
Albin Johansson <albinoscar99@hotmail.com>
2020-12-14 08:27:02 +0100

    Update ints.hpp

--- src/core/aliases/ints.hpp
+++ src/core/aliases/ints.hpp
@@ -6,18 +6,12 @@ namespace wanderer {
 
 using u64 = std::uint64_t;
 using u32 = std::uint32_t;
-// using u16 = std::uint16_t;
-// using u8 = std::uint8_t;
-//
-// using i64 = std::int64_t;
-// using i32 = std::int32_t;
-// using i16 = std::int16_t;
-// using i8 = std::int8_t;
-//
-// using ufast32 = std::uint_fast32_t;
-// using ufast64 = std::uint_fast64_t;
-//
-// using ifast32 = std::int_fast32_t;
-// using ifast64 = std::int_fast64_t;
+using u16 = std::uint16_t;
+using u8 = std::uint8_t;
+
+using i64 = std::int64_t;
+using i32 = std::int32_t;
+using i16 = std::int16_t;
+using i8 = std::int8_t;
 
 }  // namespace wanderer
