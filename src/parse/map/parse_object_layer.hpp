03f85b22683b38d4a5aef61e9695d0b0b9aa62af
Albin Johansson <albinoscar99@hotmail.com>
2020-12-15 13:48:49 +0100

    Update parse_object_layer.hpp

--- src/parse/map/parse_object_layer.hpp
+++ src/parse/map/parse_object_layer.hpp
@@ -5,6 +5,17 @@
 
 namespace wanderer {
 
+/**
+ * \brief Parses an object layer.
+ *
+ * \details Object layers contain stuff like spawnpoints, portals,
+ * containers, etc.
+ *
+ * \param registry the associated registry.
+ * \param group the object group that represents the object layer.
+ *
+ * \since 0.1.0
+ */
 void parse_object_layer(entt::registry& registry,
                         const step::object_group& group);
 
