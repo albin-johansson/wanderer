a3488ff0707154a7927ab8f753228dca6d213c0e
Albin Johansson <albinoscar99@hotmail.com>
2020-12-15 14:00:33 +0100

    Update make_tileset.hpp

--- src/parse/map/make_tileset.hpp
+++ src/parse/map/make_tileset.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <entt.hpp>
-#include <memory>  // unique_ptr
-#include <step_tileset.hpp>
-#include <vector>  // vector
+#include <entt.hpp>          // registry
+#include <memory>            // unique_ptr
+#include <step_tileset.hpp>  // tileset
+#include <vector>            // vector
 
 #include "graphics_context.hpp"
 #include "tileset.hpp"
@@ -12,6 +12,18 @@ namespace wanderer {
 
 using tileset_collection = std::vector<std::unique_ptr<step::tileset>>;
 
+/**
+ * \brief Creates a tileset based on a collection of raw tileset
+ * representations.
+ *
+ * \param registry the registry that will be associated with the tilesets.
+ * \param tilesets the collection of raw tileset representations.
+ * \param graphics the graphics context that will be used.
+ *
+ * \return the created tileset entity.
+ *
+ * \since 0.1.0
+ */
 [[nodiscard]] auto make_tileset(entt::registry& registry,
                                 const tileset_collection& tilesets,
                                 graphics_context& graphics)
