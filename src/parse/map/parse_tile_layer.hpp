9e18c4829874bc356f26e480a004507e515adaa0
Albin Johansson <albinoscar99@hotmail.com>
2020-12-15 13:52:51 +0100

    Update parse_tile_layer.hpp

--- src/parse/map/parse_tile_layer.hpp
+++ src/parse/map/parse_tile_layer.hpp
@@ -1,12 +1,23 @@
 #pragma once
 
-#include <entt.hpp>
-#include <step_map.hpp>
+#include <entt.hpp>      // registry
+#include <step_map.hpp>  // tile_layer, properties
 
 #include "tilemap.hpp"
 
 namespace wanderer {
 
+/**
+ * \brief Parses a tile layer.
+ *
+ * \param registry the associated registry.
+ * \param tilemap the tilemap component associated with the layer.
+ * \param layer the tile layer representation.
+ * \param properties the properties associated with the layer.
+ * \param index the index associated with the layer.
+ *
+ * \since 0.1.0
+ */
 void parse_tile_layer(entt::registry& registry,
                       comp::tilemap& tilemap,
                       const step::tile_layer& layer,
