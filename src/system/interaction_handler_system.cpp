b1568003dc92d326ba90a153c096e7f5d2572b0a
Albin Johansson <albinoscar99@hotmail.com>
2020-12-29 22:01:38 +0100

    Update interaction_handler_system.cpp

--- src/system/interaction_handler_system.cpp
+++ src/system/interaction_handler_system.cpp
@@ -1,11 +1,38 @@
 #include "interaction_handler_system.hpp"
 
-#include "hitbox.hpp"
+#include "close_inventory_event.hpp"
+#include "container_trigger.hpp"
 #include "null_entity.hpp"
 #include "portal.hpp"
+#include "show_inventory_event.hpp"
 #include "switch_map_event.hpp"
 
 namespace wanderer::sys {
+namespace {
+
+void enqueue_switch_map_event(entt::registry& registry,
+                              entt::dispatcher& dispatcher,
+                              const entt::entity portalEntity)
+{
+  const auto& portal = registry.get<comp::portal>(portalEntity);
+  dispatcher.enqueue<comp::switch_map_event>(portal.target.value());
+}
+
+void enqueue_inventory_event(entt::registry& registry,
+                             entt::dispatcher& dispatcher,
+                             const comp::player::entity player,
+                             const entt::entity triggerEntity)
+{
+  if (registry.empty<comp::active_inventory>()) {
+    const auto& trigger = registry.get<comp::container_trigger>(triggerEntity);
+    dispatcher.enqueue<comp::show_inventory_event>(trigger.inventoryEntity);
+
+  } else {
+    dispatcher.enqueue<comp::close_inventory_event>();
+  }
+}
+
+}  // namespace
 
 void on_interact(const comp::interact_event& event)
 {
@@ -13,12 +40,16 @@ void on_interact(const comp::interact_event& event)
   assert(event.dispatcher);
   assert(event.player != null<comp::player>());
 
-  auto* registry = event.registry;
+  auto& registry = *event.registry;
+  auto& dispatcher = *event.dispatcher;
   const auto player = event.player;
 
-  if (const auto* iwp = registry->try_get<comp::is_within_portal>(player)) {
-    const auto& portal = registry->get<comp::portal>(iwp->portalEntity);
-    event.dispatcher->enqueue<comp::switch_map_event>(portal.target.value());
+  if (const auto* p = registry.try_get<comp::is_within_portal>(player)) {
+    enqueue_switch_map_event(registry, dispatcher, p->portalEntity);
+
+  } else if (const auto* ct =
+                 registry.try_get<comp::is_within_container_trigger>(player)) {
+    enqueue_inventory_event(registry, dispatcher, player, ct->triggerEntity);
   }
 }
 
