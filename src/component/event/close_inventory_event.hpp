ce39227cb44f3c25da14ccd42167de0b28c8fe8d
Albin Johansson <albinoscar99@hotmail.com>
2020-12-29 22:27:37 +0100

    Update close_inventory_event.hpp

--- src/component/event/close_inventory_event.hpp
+++ src/component/event/close_inventory_event.hpp
@@ -1,12 +1,8 @@
 #pragma once
 
-#include "inventory.hpp"
-
 namespace wanderer::comp {
 
 struct close_inventory_event final
-{
-  comp::inventory::entity inventoryEntity{null<comp::inventory>()};
-};
+{};
 
 }  // namespace wanderer::comp
