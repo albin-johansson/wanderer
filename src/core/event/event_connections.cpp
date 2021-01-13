55eb3ba63c7759182507eb5b42d411052f2644ef
Albin Johansson <albinoscar99@hotmail.com>
2021-01-13 22:00:40 +0100

    Update event_connections.cpp

--- src/core/event/event_connections.cpp
+++ src/core/event/event_connections.cpp
@@ -10,27 +10,25 @@
 
 namespace wanderer {
 
-using namespace comp;
-
 void connect_events(entt::dispatcher& dispatcher)
 {
   using namespace sys;
   using namespace sys::humanoid;
-  dispatcher.sink<begin_attack_event>().connect<&on_attack_begin>();
-  dispatcher.sink<end_attack_event>().connect<&on_attack_end>();
-  dispatcher.sink<begin_humanoid_move_event>().connect<&on_move_begin>();
-  dispatcher.sink<end_humanoid_move_event>().connect<&on_move_end>();
-  dispatcher.sink<interact_event>().connect<&on_interact>();
+  dispatcher.sink<comp::begin_attack_event>().connect<&on_attack_begin>();
+  dispatcher.sink<comp::end_attack_event>().connect<&on_attack_end>();
+  dispatcher.sink<comp::begin_humanoid_move_event>().connect<&on_move_begin>();
+  dispatcher.sink<comp::end_humanoid_move_event>().connect<&on_move_end>();
+  dispatcher.sink<comp::interact_event>().connect<&on_interact>();
 }
 
 void disconnect_events(entt::dispatcher& dispatcher)
 {
   dispatcher.clear();
-  dispatcher.sink<end_humanoid_move_event>().disconnect();
-  dispatcher.sink<begin_humanoid_move_event>().disconnect();
-  dispatcher.sink<end_attack_event>().disconnect();
-  dispatcher.sink<begin_attack_event>().disconnect();
-  dispatcher.sink<interact_event>().disconnect();
+  dispatcher.sink<comp::end_humanoid_move_event>().disconnect();
+  dispatcher.sink<comp::begin_humanoid_move_event>().disconnect();
+  dispatcher.sink<comp::end_attack_event>().disconnect();
+  dispatcher.sink<comp::begin_attack_event>().disconnect();
+  dispatcher.sink<comp::interact_event>().disconnect();
 }
 
 }  // namespace wanderer
