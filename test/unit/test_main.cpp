308f47f4cd00ce02e6778ecb3e356e3d56ec6ba7
Albin Johansson <albinoscar99@hotmail.com>
2021-02-15 10:43:19 +0100

    Update test_main.cpp

--- test/unit/test_main.cpp
+++ test/unit/test_main.cpp
@@ -1,8 +1,10 @@
 #define CATCH_CONFIG_RUNNER
 
 #include <catch.hpp>
+#include <cen/centurion.hpp>
 
 int main(int argc, char** argv)
 {
+  cen::library lib;
   return Catch::Session().run(argc, argv);
 }
