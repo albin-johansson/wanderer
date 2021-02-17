29a11c67bd1b4568c70ef90e37e20042bba376fd
Albin Johansson <albinoscar99@hotmail.com>
2021-02-17 23:16:20 +0100

    Update portal.hpp

--- src/component/portal.hpp
+++ src/component/portal.hpp
@@ -16,8 +16,8 @@ struct portal_entity_t;
 struct portal final
 {
   using entity = entity_type<detail::portal_entity_t>;
-  std::filesystem::path path;  ///< Path to target map file.
-  maybe<map_id> target;        ///< Identifier associated with target map.
+  std::string path;      ///< Path to target map file.
+  maybe<map_id> target;  ///< Identifier associated with target map.
 };
 
 struct is_within_portal final
