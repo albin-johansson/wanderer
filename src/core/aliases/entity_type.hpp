0ca5f0d9968fca6aaccd39fb558d0f439809dab1
Albin Johansson <albinoscar99@hotmail.com>
2021-02-17 18:47:32 +0100

    Update entity_type.hpp

--- src/core/aliases/entity_type.hpp
+++ src/core/aliases/entity_type.hpp
@@ -10,17 +10,3 @@ using entity_type = fluent::
     NamedType<entt::entity, T, fluent::Comparable, fluent::FunctionCallable>;
 
 }  // namespace wanderer
-
-namespace fluent {
-
-template <typename Archive,
-          typename T,
-          typename Parameter,
-          template <typename>
-          class... Skills>
-void serialize(Archive& archive, NamedType<T, Parameter, Skills...>& n)
-{
-  archive(n.get());
-}
-
-}  // namespace fluent
