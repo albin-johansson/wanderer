8ab8668747a3fb9f92d3997f825c076832aa4aa4
Albin Johansson <albinoscar99@hotmail.com>
2021-02-23 17:00:44 +0100

    Update menu_constants.hpp

--- src/core/menu_constants.hpp
+++ src/core/menu_constants.hpp
@@ -6,13 +6,12 @@
 
 namespace wanderer::glob {
 
-inline constexpr entt::hashed_string menu_font_xs{"typewriter_xs"};
 inline constexpr entt::hashed_string menu_font_s{"typewriter_s"};
 inline constexpr entt::hashed_string menu_font_m{"typewriter_m"};
 inline constexpr entt::hashed_string menu_font_l{"typewriter_l"};
 
-inline constexpr int menu_row_size = 20;
-inline constexpr int menu_col_size = 20;
+inline constexpr int menu_row_size = 30;
+inline constexpr int menu_col_size = 30;
 
 inline constexpr int menu_rows = logical_height<int> / menu_row_size;
 inline constexpr int menu_columns = logical_width<int> / menu_col_size;
