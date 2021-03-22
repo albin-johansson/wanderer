eb7b5a727af0d9371d21aee4807b409475e14206
Albin Johansson <albinoscar99@hotmail.com>
2021-03-22 21:33:10 +0100

    Upgrade to WIP Centurion 6

--- src/io/files_directory.cpp
+++ src/io/files_directory.cpp
@@ -7,7 +7,7 @@ namespace wanderer {
 auto files_directory() -> const std::filesystem::path&
 {
   static std::filesystem::path path =
-      cen::get_pref_path("albin-johansson", "wanderer").copy();
+      cen::preferred_path("albin-johansson", "wanderer").copy();
   return path;
 }
 
