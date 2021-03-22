88796a9259794a773a04a93eb8c2fb363184326d
Albin Johansson <albinoscar99@hotmail.com>
2021-03-22 11:45:05 +0100

    Update render_text.cpp

--- src/core/render_text.cpp
+++ src/core/render_text.cpp
@@ -6,7 +6,7 @@ auto render_text(cen::renderer& renderer,
                  const std::string& text,
                  const cen::font& font) -> cen::texture
 {
-  return renderer.render_blended_utf8(text.c_str(), font);
+  return renderer.render_blended_utf8(text, font);
 }
 
 }  // namespace wanderer
