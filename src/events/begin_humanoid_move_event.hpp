1965782d47f9a2c493186aa05a498ab45e137078
Albin Johansson <albinoscar99@hotmail.com>
2021-04-02 10:48:34 +0200

    Update begin_humanoid_move_event.hpp

--- src/events/begin_humanoid_move_event.hpp
+++ src/events/begin_humanoid_move_event.hpp
@@ -7,18 +7,20 @@
 namespace wanderer::event {
 
 /**
- * \struct begin_humanoid_move_event
+ * \struct begin_humanoid_move
  *
  * \brief Used for events dispatched when a humanoid starts to move.
  *
  * \note This is an event component and is meant to be dispatched through
  * `entt::dispatcher::enqueue`.
  *
- * \var begin_humanoid_move_event::registry
+ * \var begin_humanoid_move::registry
  * A pointer to the associated registry.
- * \var begin_humanoid_move_event::entity
+ *
+ * \var begin_humanoid_move::entity
  * The humanoid that should start moving.
- * \var begin_humanoid_move_event::direction
+ *
+ * \var begin_humanoid_move::direction
  * The direction in which the humanoid should move in.
  *
  * \headerfile begin_humanoid_move_event.hpp
