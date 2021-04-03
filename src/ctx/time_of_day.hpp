df40e06e0a727f83c7ad5175eea0ec76beb26614
Albin Johansson <albinoscar99@hotmail.com>
2021-04-03 18:37:59 +0200

    Update time_of_day.hpp

--- src/ctx/time_of_day.hpp
+++ src/ctx/time_of_day.hpp
@@ -8,9 +8,9 @@ namespace wanderer::ctx {
 
 struct time_of_day final
 {
-  float hour{13};    ///< [0, 24)
-  float minute{0};   ///< [0, 60)
-  float seconds{0};  ///< [0, 60)
+  float hour{};     ///< [0, 24)
+  float minute{};   ///< [0, 60)
+  float seconds{};  ///< This has no predefined range, it is reset once per in-game day
 
   u8 opacity{};
   cen::color color;
