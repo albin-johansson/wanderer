1463622fe88f1993a3d2806e061d2250a1b8cb7e
Albin Johansson <albinoscar99@hotmail.com>
2021-04-03 18:39:01 +0200

    Update time_of_day_system.hpp

--- src/systems/rendering/time_of_day_system.hpp
+++ src/systems/rendering/time_of_day_system.hpp
@@ -9,9 +9,6 @@ namespace wanderer::sys {
 
 void update_time_of_day(entt::registry& registry, delta_t dt);
 
-void render_time_of_day_overlay(const entt::registry& registry,
-                                graphics_context& graphics);
-
 void render_clock(const entt::registry& registry, graphics_context& graphics);
 
 }  // namespace wanderer::sys
