dfd756f6041c3cacf2aa4f1c9e5cc1798ff3c394
Albin Johansson <albinoscar99@hotmail.com>
2021-04-05 13:03:18 +0200

    Update create_controls_menu.cpp

--- src/systems/ui/menus/create_controls_menu.cpp
+++ src/systems/ui/menus/create_controls_menu.cpp
@@ -2,7 +2,10 @@
 
 #include "button.hpp"
 #include "button_system.hpp"
+#include "grid_system.hpp"
 #include "key_bind_system.hpp"
+#include "label.hpp"
+#include "label_system.hpp"
 #include "menu_system.hpp"
 
 using namespace entt::literals;
@@ -20,6 +23,20 @@ void add_buttons(entt::registry& registry, const comp::menu::entity entity)
   // clang-format on
 }
 
+void add_labels(entt::registry& registry, const comp::menu::entity entity)
+{
+  auto& pack = registry.emplace<comp::label_pack>(entity);
+  auto& labels = pack.labels;
+
+  constexpr auto x = column_to_x(13);
+  labels.push_back(sys::make_label(registry, "Move Up", x, row_to_y(6)));
+  labels.push_back(sys::make_label(registry, "Move Down", x, row_to_y(7)));
+  labels.push_back(sys::make_label(registry, "Move Left", x, row_to_y(8)));
+  labels.push_back(sys::make_label(registry, "Move Right", x, row_to_y(9)));
+  labels.push_back(sys::make_label(registry, "Attack", x, row_to_y(10)));
+  labels.push_back(sys::make_label(registry, "Interact", x, row_to_y(11)));
+}
+
 }  // namespace
 
 auto create_controls_menu(entt::registry& registry) -> comp::menu::entity
@@ -28,6 +45,7 @@ auto create_controls_menu(entt::registry& registry) -> comp::menu::entity
   registry.set<comp::controls_menu>(entity);
 
   add_buttons(registry, entity);
+  add_labels(registry, entity);
   add_binds(registry,
             entity,
             comp::key_bind{cen::scancodes::escape, menu_action::goto_home});
