4a5e6b5e4ee06fa37701ef4f7807e1fc36db77c1
Albin Johansson <albinoscar99@hotmail.com>
2021-04-05 16:06:02 +0200

    Update saves_system.cpp

--- src/systems/saves/saves_system.cpp
+++ src/systems/saves/saves_system.cpp
@@ -1,24 +1,31 @@
 #include "saves_system.hpp"
 
+#include "active_menu.hpp"
 #include "animated.hpp"
 #include "animated_tile.hpp"
 #include "binds.hpp"
 #include "chase.hpp"
 #include "container_trigger.hpp"
 #include "depth_drawable.hpp"
+#include "float3.hpp"
 #include "hitbox.hpp"
 #include "humanoid_state.hpp"
 #include "inventory.hpp"
 #include "movable.hpp"
 #include "object.hpp"
+#include "outside_level.hpp"
+#include "particle.hpp"
 #include "player.hpp"
+#include "point_light.hpp"
 #include "portal.hpp"
+#include "render_bounds.hpp"
 #include "spawnpoint.hpp"
 #include "tile.hpp"
 #include "tile_layer.hpp"
 #include "tile_object.hpp"
 #include "tilemap.hpp"
 #include "tileset.hpp"
+#include "time_of_day.hpp"
 #include "viewport.hpp"
 
 namespace wanderer::sys {
@@ -28,14 +35,20 @@ template <typename T, typename Archive>
 void process(T& handle, Archive& archive)
 {
   handle.entities(archive)
-      .template component<ctx::tileset,
+      .template component<ctx::active_menu,
                           ctx::binds,
+                          ctx::player,
+                          ctx::outside_level,
+                          ctx::render_bounds,
+                          ctx::tileset,
+                          ctx::time_of_day,
                           ctx::viewport,
                           comp::animated,
                           comp::animated_tile,
                           comp::chase,
                           comp::container_trigger,
                           comp::depth_drawable,
+                          comp::float3,
                           comp::hitbox,
                           comp::humanoid,
                           comp::humanoid_idle,
@@ -45,8 +58,9 @@ void process(T& handle, Archive& archive)
                           comp::inventory,
                           comp::movable,
                           comp::object,
-                          ctx::player,
+                          comp::particle,
                           comp::portal,
+                          comp::point_light,
                           comp::spawnpoint,
                           comp::tile,
                           comp::tile_layer,
