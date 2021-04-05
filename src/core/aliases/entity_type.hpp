9a341c4426ff5f3ea5c21337de3a3c270508b3be
Albin Johansson <albinoscar99@hotmail.com>
2021-04-05 13:02:46 +0200

    Update entity_type.hpp

--- src/core/aliases/entity_type.hpp
+++ src/core/aliases/entity_type.hpp
@@ -9,4 +9,32 @@ template <typename T>
 using entity_type =
     fluent::NamedType<entt::entity, T, fluent::Comparable, fluent::FunctionCallable>;
 
+template <typename T>
+[[nodiscard]] constexpr auto operator==(const entity_type<T> e, const entt::null_t null)
+    -> bool
+{
+  return e.get() == null;
+}
+
+template <typename T>
+[[nodiscard]] constexpr auto operator==(const entt::null_t null, const entity_type<T> e)
+    -> bool
+{
+  return e == null;
+}
+
+template <typename T>
+[[nodiscard]] constexpr auto operator!=(const entity_type<T> e, const entt::null_t null)
+    -> bool
+{
+  return e.get() != null;
+}
+
+template <typename T>
+[[nodiscard]] constexpr auto operator!=(const entt::null_t null, const entity_type<T> e)
+    -> bool
+{
+  return e != null;
+}
+
 }  // namespace wanderer
