d8dc440c800d7396e0b70759e5f0698389edc757
Albin Johansson <albinoscar99@hotmail.com>
2021-04-10 01:41:47 +0200

    Update debug_rendering_system.cpp

--- src/systems/graphics/debug_rendering_system.cpp
+++ src/systems/graphics/debug_rendering_system.cpp
@@ -75,6 +75,19 @@ void render_chase_ranges(const entt::registry& registry, cen::renderer& renderer
   });
 }
 
+
+void render_development_build_label(graphics_context& graphics)
+{
+  auto& renderer = graphics.renderer();
+  const auto& cache = graphics.small_font_cache();
+
+  const char str[] = "Development build";
+  const auto [width, height] = cache.get_font().string_size(str);
+  const auto x = glob::logical_width<int> - width - 6;
+  const auto y = glob::logical_height<int> - height - 6;
+  renderer.render_text(cache, str, {x, y});
+}
+
 }  // namespace
 
 void render_debug_info(const entt::registry& registry, graphics_context& graphics)
@@ -114,6 +127,8 @@ void render_menu_debug_info(const entt::registry& registry, graphics_context& gr
     renderer.draw_line<int>({0, endY - 1}, {endX, endY - 1});
     renderer.draw_line<int>({endX - 1, 0}, {endX - 1, endY});
   }
+
+  render_development_build_label(graphics);
 }
 
 }  // namespace wanderer::sys
