aa204037e42d1c3f4bb00725166b798c9236b4ff
Albin Johansson <albinoscar99@hotmail.com>
2021-04-14 22:18:01 +0200

    Update depth_system.cpp

--- src/systems/graphics/depth_system.cpp
+++ src/systems/graphics/depth_system.cpp
@@ -5,18 +5,23 @@
 #include "depth_drawable.hpp"
 
 namespace wanderer::sys {
+namespace {
+
+[[nodiscard]] auto sort(const comp::depth_drawable& lhs,
+                        const comp::depth_drawable& rhs) noexcept -> bool
+{
+  const auto lhsCenterY = lhs.dst.center_y();
+  const auto rhsCenterY = rhs.dst.center_y();
+
+  return std::tie(lhs.layer, lhs.depth, lhsCenterY) <
+         std::tie(rhs.layer, rhs.depth, rhsCenterY);
+}
+
+}  // namespace
 
 void update_depth(entt::registry& registry)
 {
-  registry.sort<comp::depth_drawable>(
-      [](const comp::depth_drawable& lhs, const comp::depth_drawable& rhs) noexcept {
-        const auto lhsCenterY = lhs.dst.center_y();
-        const auto rhsCenterY = rhs.dst.center_y();
-
-        return std::tie(lhs.layer, lhs.depth, lhsCenterY) <
-               std::tie(rhs.layer, rhs.depth, rhsCenterY);
-      },
-      entt::insertion_sort{});
+  registry.sort<comp::depth_drawable>(sort, entt::insertion_sort{});
 }
 
 }  // namespace wanderer::sys
