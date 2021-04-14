ee1a631813c6c6fe64fdd2fe588db4b260205909
Albin Johansson <albinoscar99@hotmail.com>
2021-04-14 18:51:16 +0200

    Update drawable_system.cpp

--- src/systems/graphics/drawable_system.cpp
+++ src/systems/graphics/drawable_system.cpp
@@ -10,22 +10,24 @@ namespace wanderer::sys {
 void update_drawables(entt::registry& registry)
 {
   const auto view = registry.view<const comp::movable, comp::depth_drawable>();
-  view.each([](const comp::movable& movable, comp::depth_drawable& drawable) noexcept {
+  for (auto&& [entity, movable, drawable] : view.each())
+  {
     drawable.dst.set_x(movable.position.x);
     drawable.dst.set_y(movable.position.y);
-  });
+  }
 }
 
 void render_drawables(const entt::registry& registry, graphics_context& graphics)
 {
-  const auto boundsRect = to_rect(registry.ctx<ctx::render_bounds>());
+  const auto bounds = to_rect(registry.ctx<ctx::render_bounds>());
   const auto view = registry.view<const comp::depth_drawable>();
-  view.each([&](const comp::depth_drawable& drawable) noexcept {
-    if (cen::intersects(boundsRect, drawable.dst))
+  for (auto&& [entity, drawable] : view.each())
+  {
+    if (cen::intersects(bounds, drawable.dst))
     {
       graphics.render(drawable.texture, drawable.src, drawable.dst);
     }
-  });
+  }
 }
 
 }  // namespace wanderer::sys
