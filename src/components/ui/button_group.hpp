ae6cc4a653991706a04359c3fe5c8efe6ebd0d0f
Albin Johansson <albinoscar99@hotmail.com>
2021-04-16 00:25:03 +0200

    Update button_group.hpp

--- src/components/ui/button_group.hpp
+++ src/components/ui/button_group.hpp
@@ -4,6 +4,8 @@
 
 #include "button.hpp"
 #include "entity_type.hpp"
+#include "label.hpp"
+#include "null_entity.hpp"
 
 namespace wanderer::comp {
 namespace detail {
@@ -15,7 +17,12 @@ struct button_group final
   using entity = entity_type<detail::button_group_entity_t>;
 
   std::vector<button::entity> buttons;
-  button::entity selected;
+
+  button::entity selected{null<button>()};
+  label::entity indicatorLabel{null<label>()};  ///< Reference to associated label
+
+  int currentPage{};   ///< The index of the current page
+  int itemsPerPage{};  ///< The maximum amount of buttons per page
 };
 
 }  // namespace wanderer::comp
