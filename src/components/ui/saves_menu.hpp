66630d6009c8a5eb583ab893b479de59fb610dea
Albin Johansson <albinoscar99@hotmail.com>
2021-04-16 00:25:14 +0200

    Update saves_menu.hpp

--- src/components/ui/saves_menu.hpp
+++ src/components/ui/saves_menu.hpp
@@ -3,7 +3,9 @@
 #include <string>  // string
 #include <vector>  // vector
 
+#include "button.hpp"
 #include "entity_type.hpp"
+#include "null_entity.hpp"
 
 namespace wanderer::comp {
 namespace detail {
@@ -23,6 +25,11 @@ struct saves_menu final
   using entity = entity_type<detail::saves_menu_entity_t>;
 
   std::vector<saves_menu_entry::entity> entries;
+
+  button::entity decrementButton{null<button>()};
+  button::entity incrementButton{null<button>()};
+  button::entity deleteButton{null<button>()};
+  button::entity loadButton{null<button>()};
 };
 
 }  // namespace wanderer::comp
