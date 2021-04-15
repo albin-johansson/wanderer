82867fa782ca5e2d39a9d92c77a12543d060dbef
Albin Johansson <albinoscar99@hotmail.com>
2021-04-15 16:33:58 +0200

    Add common_concepts.hpp

---
+++ src/core/common_concepts.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <concepts>  // convertible_to
+#include <ranges>    // range
+
+namespace wanderer {
+
+// clang-format off
+
+template <typename T, typename Elem>
+concept has_value_type = requires (T t)
+{
+  typename T::value_type;
+  std::convertible_to<typename T::value_type, Elem>;
+};
+
+// An iterable container with a value type convertible to the specified element type
+template <typename T, typename Elem>
+concept container_type = std::ranges::range<T> &&
+                         has_value_type<T, Elem>;
+
+// clang-format on
+
+}  // namespace wanderer
