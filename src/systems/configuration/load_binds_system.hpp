ed54f92f381b1eb3b38d55a9d18fd8b584990ae2
Albin Johansson <albinoscar99@hotmail.com>
2021-04-17 21:11:54 +0200

    Update load_binds_system.hpp

--- src/systems/configuration/load_binds_system.hpp
+++ src/systems/configuration/load_binds_system.hpp
@@ -7,6 +7,16 @@ namespace wanderer::sys {
 /// \name Configuration
 /// \{
 
+/**
+ * \brief Attempts to read the binds from the preferred file directory.
+ *
+ * \ingroup systems
+ *
+ * \details This function will look for a binds file called `binds.ini` in the preferred
+ * file directory. The default binds will be used if no such file was found.
+ *
+ * \return the parsed binds.
+ */
 [[nodiscard]] auto load_binds() -> ctx::binds;
 
 /// \} End of configuration
