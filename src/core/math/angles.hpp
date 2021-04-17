885810d337bad043e4ceaa624695489360fbb16f
Albin Johansson <albinoscar99@hotmail.com>
2021-04-17 20:12:24 +0200

    Update angles.hpp

--- src/core/math/angles.hpp
+++ src/core/math/angles.hpp
@@ -4,6 +4,9 @@
 
 namespace wanderer {
 
+/// \addtogroup core
+/// \{
+
 inline constexpr auto pi = std::numbers::pi_v<float>;
 
 [[nodiscard]] inline constexpr auto to_degrees(const float radians) noexcept -> float
@@ -15,5 +18,7 @@ inline constexpr auto pi = std::numbers::pi_v<float>;
 {
   return (degrees * pi) / 180.0f;
 }
+
+/// \} End of group core
 
 }  // namespace wanderer
