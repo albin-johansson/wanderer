0f4b0e9e814928f88f0a1e48a21071708399a2ab
Albin Johansson <albinoscar99@hotmail.com>
2021-04-18 00:18:38 +0200

    Update interaction_system.hpp

--- src/systems/interaction_system.hpp
+++ src/systems/interaction_system.hpp
@@ -7,6 +7,16 @@ namespace wanderer::sys {
 /// \name Event handlers
 /// \{
 
+/**
+ * \brief Handles an interaction event.
+ *
+ * \details This function will process the supplied event and subsequently potentially
+ * emit more specific events, depending on the state of the player.
+ *
+ * \ingroup systems
+ *
+ * \param event the event data.
+ */
 void on_interact(const interact_event& event);
 
 /// \} End of event handlers
