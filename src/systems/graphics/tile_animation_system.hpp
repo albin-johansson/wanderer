ab086de0f36cf0f6e64c85cfc3464803017f4faf
Albin Johansson <albinoscar99@hotmail.com>
2021-04-17 21:39:21 +0200

    Update tile_animation_system.hpp

--- src/systems/graphics/tile_animation_system.hpp
+++ src/systems/graphics/tile_animation_system.hpp
@@ -1,12 +1,19 @@
 #pragma once
 
-#include <entt.hpp>
+#include <entt.hpp>  // registry
 
 namespace wanderer::sys {
 
 /// \name Tiles
 /// \{
 
+/**
+ * \brief Updates the state of all tile animations.
+ *
+ * \ingroup systems
+ *
+ * \param registry the current level registry.
+ */
 void update_tile_animations(entt::registry& registry);
 
 /// \} End of tiles
