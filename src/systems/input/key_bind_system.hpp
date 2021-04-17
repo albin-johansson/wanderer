ed90fe72b7f5cfabe488cd44b036b386e082759c
Albin Johansson <albinoscar99@hotmail.com>
2021-04-17 23:24:30 +0200

    Update key_bind_system.hpp

--- src/systems/input/key_bind_system.hpp
+++ src/systems/input/key_bind_system.hpp
@@ -12,14 +12,27 @@ namespace wanderer::sys {
 /// \name Input
 /// \{
 
+/**
+ * \brief Adds a key bind pack to an entity.
+ *
+ * \details This is a terse convenience function for creating key bind packs.
+ *
+ * \ingroup systems
+ *
+ * \tparam T the pack of `comp::key_bind` instances.
+ *
+ * \param registry the registry that contains the menus.
+ * \param entity the entity that will have an `comp::key_bind_pack` added to it.
+ * \param binds the pack of key binds.
+ */
 template <typename... T>
-void add_binds(entt::registry& registry, const entt::entity entity, T... bind)
+void add_binds(entt::registry& registry, const entt::entity entity, T... binds)
 {
   static_assert((std::is_same_v<T, comp::key_bind>, ...));
 
   auto& pack = registry.emplace<comp::key_bind_pack>(entity);
 
-  const auto addBind = [&](comp::key_bind bind) {
+  const auto addBind = [&](const comp::key_bind bind) {
     const auto entity = comp::key_bind::entity{registry.create()};
 
     registry.emplace<comp::key_bind>(entity, bind);
@@ -27,9 +40,20 @@ void add_binds(entt::registry& registry, const entt::entity entity, T... bind)
     return entity;
   };
 
-  (pack.binds.push_back(addBind(bind)), ...);
+  (pack.binds.push_back(addBind(binds)), ...);
 }
 
+/**
+ * \brief Creates a key bind entity.
+ *
+ * \ingroup systems
+ *
+ * \param registry the shared data registry that contains the menus.
+ * \param key the key that will trigger the bind.
+ * \param action the action that will be associated with the bind.
+ *
+ * \return the created key bind entity.
+ */
 auto make_bind(entt::registry& registry, cen::scan_code key, menu_action action)
     -> comp::key_bind::entity;
 
