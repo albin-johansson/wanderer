f322436793d8cf0495bfaed88849cc296e236cb3
Albin Johansson <albinoscar99@hotmail.com>
2021-04-17 20:12:58 +0200

    Update menu_id.hpp

--- src/core/menu_id.hpp
+++ src/core/menu_id.hpp
@@ -2,13 +2,15 @@
 
 namespace wanderer {
 
+/// \brief Provides identifiers for all of the different game menus.
+/// \ingroup core
 enum class menu_id
 {
-  in_game,
-  home,
-  settings,
-  saves,
-  controls
+  in_game,   ///< The in-game "menu", i.e. the menu active when the game is played.
+  home,      ///< The home (or start) menu.
+  settings,  ///< The settings menu.
+  saves,     ///< The saves management menu.
+  controls   ///< The controls configuration menu.
 };
 
 }  // namespace wanderer
