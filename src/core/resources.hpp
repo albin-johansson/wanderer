a12b6807adf5360f827a64ec17b562527e4ada51
Albin Johansson <albinoscar99@hotmail.com>
2021-04-17 20:19:51 +0200

    Update resources.hpp

--- src/core/resources.hpp
+++ src/core/resources.hpp
@@ -8,11 +8,10 @@
 /// \ingroup core
 namespace wanderer::resources {
 
-/// \addtogroup core
-/// \{
-
 /**
  * \brief Returns the file path for a texture with the specified name.
+ *
+ * \ingroup core
  *
  * \param resource the name of the resource.
  *
@@ -23,6 +22,8 @@ namespace wanderer::resources {
 /**
  * \brief Returns the file path for a font with the specified name.
  *
+ * \ingroup core
+ *
  * \param resource the name of the resource.
  *
  * \return the path of the font.
@@ -31,6 +32,8 @@ namespace wanderer::resources {
 
 /**
  * \brief Returns the file path for a sound with the specified name.
+ *
+ * \ingroup core
  *
  * \param resource the name of the resource.
  *
@@ -41,12 +44,12 @@ namespace wanderer::resources {
 /**
  * \brief Returns the file path for a map with the specified name.
  *
+ * \ingroup core
+ *
  * \param resource the name of the resource.
  *
  * \return the path of the map.
  */
 [[nodiscard]] auto map(cen::czstring resource) -> std::string;
-
-/// \} End of group core
 
 }  // namespace wanderer::resources
