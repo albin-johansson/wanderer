787bdbca5870a1061ebf5ce129d5b2dd4b09281f
Albin Johansson <albinoscar99@hotmail.com>
2021-04-17 21:11:33 +0200

    Update drawable_system.hpp

--- src/systems/graphics/drawable_system.hpp
+++ src/systems/graphics/drawable_system.hpp
@@ -9,10 +9,19 @@ namespace wanderer::sys {
 /// \name Drawables
 /// \{
 
+/**
+ * \brief Updates the destination rectangles of all drawables that are movable.
+ *
+ * \ingroup systems
+ *
+ * \param registry the current level registry.
+ */
 void update_drawables(entt::registry& registry);
 
 /**
  * \brief Renders all drawable objects.
+ *
+ * \ingroup systems
  *
  * \note Make sure to call `sys::update_depth()` before calling this function.
  *
