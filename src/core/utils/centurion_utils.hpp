4780a95f7e051147da276966d88b7b288de6dc21
Albin Johansson <albinoscar99@hotmail.com>
2021-04-17 20:19:43 +0200

    Update centurion_utils.hpp

--- src/core/utils/centurion_utils.hpp
+++ src/core/utils/centurion_utils.hpp
@@ -6,33 +6,63 @@
 
 namespace wanderer {
 
-/// \addtogroup core
+/// \name Centurion utilities
 /// \{
 
-/// Converts a two-dimensional vector to a Centurion point
+/**
+ * \brief Converts a two-dimensional vector to a Centurion point.
+ *
+ * \ingroup core
+ *
+ * \tparam T the precision type of the vector.
+ *
+ * \param vector the vector that will be converted
+ *
+ * \return a corresponding Centurion point.
+ */
 template <typename T>
-[[nodiscard]] constexpr auto to_point(const basic_vector2<T>& vector) noexcept
+[[nodiscard]] constexpr auto to_point(const basic_vector2<T> vector) noexcept
     -> cen::basic_point<T>
 {
   return cen::basic_point<T>{vector.x, vector.y};
 }
 
-/// Converts a Centurion point to a two-dimensional vector
+/**
+ * \brief Converts a Centurion point to a two-dimensional vector.
+ *
+ * \ingroup core
+ *
+ * \tparam T the precision type of the point.
+ *
+ * \param point the point that will be converted.
+ *
+ * \return a corresponding vector.
+ */
 template <typename T>
-[[nodiscard]] constexpr auto to_vector(const cen::basic_point<T>& point) noexcept
+[[nodiscard]] constexpr auto to_vector(const cen::basic_point<T> point) noexcept
     -> basic_vector2<T>
 {
   return basic_vector2<T>{point.x(), point.y()};
 }
 
-/// Converts a Centurion area to a two-dimensional vector
+/**
+ * \brief Converts a Centurion area to a two-dimensional vector.
+ *
+ * \ingroup core
+ *
+ * \tparam T the precision type of the point.
+ *
+ * \param area the area that will be converted.
+ *
+ * \return a corresponding vector.
+ */
 template <typename T>
-[[nodiscard]] constexpr auto to_vector(const cen::basic_area<T>& area) noexcept
+[[nodiscard]] constexpr auto to_vector(const cen::basic_area<T> area) noexcept
     -> basic_vector2<T>
 {
   return basic_vector2<T>{area.width, area.height};
 }
 
-/// \} End of group core
+/// \} End of centurion utilities
 
 }  // namespace wanderer
