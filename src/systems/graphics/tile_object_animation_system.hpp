518c7861f53fb4983d1c1c6c0dbfe2d2ea4d7dbc
Albin Johansson <albinoscar99@hotmail.com>
2021-04-17 21:44:35 +0200

    Update tile_object_animation_system.hpp

--- src/systems/graphics/tile_object_animation_system.hpp
+++ src/systems/graphics/tile_object_animation_system.hpp
@@ -10,6 +10,13 @@ namespace wanderer::sys {
 /**
  * \brief Updates the animations of all tile objects.
  *
+ * \details This function iterates all entities with both `tile_object` and
+ * `depth_drawable` components, and if any of the entities associated with a `tile_object`
+ * has a `tile_animation` component, then the `depth_drawable` is updated to render the
+ * correct part of the associated tile sheet.
+ *
+ * \ingroup systems
+ *
  * \param registry the current level registry.
  */
 void update_tile_object_animations(entt::registry& registry);
