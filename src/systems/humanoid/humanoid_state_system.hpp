e1f0086b4d9c5a7debbb77b2ddc9210e73b975e1
Albin Johansson <albinoscar99@hotmail.com>
2021-04-17 23:17:21 +0200

    Update humanoid_state_system.hpp

--- src/systems/humanoid/humanoid_state_system.hpp
+++ src/systems/humanoid/humanoid_state_system.hpp
@@ -1,12 +1,20 @@
 #pragma once
 
-#include <entt.hpp>
+#include <entt.hpp>  // registry, dispatcher
 
 namespace wanderer::sys {
 
 /// \name Humanoids
 /// \{
 
+/**
+ * \brief Updates the state of all humanoids.
+ *
+ * \ingroup systems
+ *
+ * \param registry the current level registry.
+ * \param dispatcher the dispatcher that will be used.
+ */
 void update_humanoid_states(entt::registry& registry, entt::dispatcher& dispatcher);
 
 /// \} End of humanoid
