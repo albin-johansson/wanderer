93a4b610c5f84c975694daea8d9afdcd2a9280fd
Albin Johansson <albinoscar99@hotmail.com>
2021-04-17 20:13:19 +0200

    Update chase_system.hpp

--- src/systems/ai/chase_system.hpp
+++ src/systems/ai/chase_system.hpp
@@ -7,6 +7,14 @@ namespace wanderer::sys {
 /// \name AI
 /// \{
 
+/**
+ * \brief Updates entities with `chase` components.
+ *
+ * \ingroup systems
+ *
+ * \param registry the current level registry.
+ * \param dispatcher the dispatcher that will be used.
+ */
 void update_chase(entt::registry& registry, entt::dispatcher& dispatcher);
 
 /// \} End of AI
