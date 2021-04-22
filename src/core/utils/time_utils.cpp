68c15dab6ed71e6bebd752ece3fdde6aa3759326
Albin Johansson <albinoscar99@hotmail.com>
2021-04-23 00:37:52 +0200

    Add time_utils

---
+++ src/core/utils/time_utils.cpp
@@ -0,0 +1,37 @@
+#include "time_utils.hpp"
+
+#include <ctime>  // ctime_s
+
+using namespace std::chrono;
+
+using file_clock = std::filesystem::file_time_type::clock;
+
+namespace wanderer {
+
+// TODO when MSVC implements the date and time zone additions to std::chrono, use those
+//   instead of the old C-style APIs
+
+auto file_modification_time(const std::filesystem::path& path) -> std::string
+{
+  const auto writeTime = std::filesystem::last_write_time(path);
+
+  const auto timePoint = time_point_cast<system_clock::duration>(
+      system_clock::now() + (writeTime - file_clock::now()));
+
+  const auto time = system_clock::to_time_t(timePoint);
+
+  /* Note, ctime is specified to return a string using a format using a total of 24
+     characters. However, the MSVC documentation states that the buffer of ctime_s *must*
+     be large enough to hold at least 26 characters. */
+  char buffer[26];
+  if (ctime_s(buffer, sizeof buffer, &time) == 0)  // ctime_s is an MSVC extension
+  {
+    return std::string{buffer, 24};
+  }
+  else
+  {
+    return "N/A";
+  }
+}
+
+}  // namespace wanderer
---
+++ src/core/utils/time_utils.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <filesystem>  // path
+#include <string>      // string
+
+namespace wanderer {
+
+/**
+ * \brief Returns a textual representation of the time that the specified file was
+ * modified.
+ *
+ * \ingroup core
+ *
+ * \param path the file that will be checked.
+ *
+ * \return a textual representation of the last write time of the specified file.
+ */
+[[nodiscard]] auto file_modification_time(const std::filesystem::path& path)
+    -> std::string;
+
+}  // namespace wanderer
