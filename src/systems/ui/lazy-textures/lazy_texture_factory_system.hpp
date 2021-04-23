3533cfeb931c069723de8274e1cfdfba33b67e94
Albin Johansson <albinoscar99@hotmail.com>
2021-04-23 23:15:47 +0200

    Update lazy_texture_factory_system.hpp

--- src/systems/ui/lazy-textures/lazy_texture_factory_system.hpp
+++ src/systems/ui/lazy-textures/lazy_texture_factory_system.hpp
@@ -9,10 +9,31 @@
 
 namespace wanderer::sys {
 
+/// \name UI
+/// \{
+
+/**
+ * \brief Creates and returns a lazy texture entity.
+ *
+ * \details The created entity will feature `comp::lazy_texture` and
+ * `comp::associated_menu` components.
+ *
+ * \ingroup systems
+ *
+ * \param registry the menu registry.
+ * \param menu the associated menu entity.
+ * \param surface the source image data.
+ * \param position the position of the texture.
+ * \param size the rendered size of the texture.
+ *
+ * \return the created lazy texture entity.
+ */
 auto make_lazy_texture(entt::registry& registry,
                        comp::menu::entity menu,
                        cen::surface surface,
                        grid_position position,
                        cen::farea size) -> comp::lazy_texture::entity;
 
-}
+/// \} End of UI
+
+}  // namespace wanderer::sys
