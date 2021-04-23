451dceaddf78b66a515373e9c1f9226a467c8b01
Albin Johansson <albinoscar99@hotmail.com>
2021-04-23 23:51:54 +0200

    Update saves_menu_system.hpp

--- src/systems/ui/menus/saves/saves_menu_system.hpp
+++ src/systems/ui/menus/saves/saves_menu_system.hpp
@@ -9,15 +9,48 @@ namespace wanderer::sys {
 /// \name Menus
 /// \{
 
-/// Refreshes the state of the saves menu, e.g. fetches the currently available save files
+/**
+ * \brief Refreshes the state of the saves menu.
+ *
+ * \details This function updates the saves menu by fetching the currently available save
+ * files. This is meant to be called just before the saves menu is enabled.
+ *
+ * \ingroup systems
+ *
+ * \param registry the menu registry.
+ */
 void update_saves_menu(entt::registry& registry);
 
+/**
+ * \brief Updates the save file preview.
+ *
+ * \details This destroys the current temporary entities used for save file previews, and
+ * replaces them with new ones based on the currently selected save file.
+ *
+ * \pre The saves menu must be active.
+ *
+ * \ingroup systems
+ *
+ * \param registry the menu registry.
+ */
 void change_save_preview(entt::registry& registry);
 
-/// Increments the current saves button group page index
+/**
+ * \brief Changes the current save file page to the next one.
+ *
+ * \ingroup systems
+ *
+ * \param registry the menu registry.
+ */
 void increment_saves_button_group_page(entt::registry& registry);
 
-/// Decrements the current saves button group page index
+/**
+ * \brief Changes the current save file page to the previous one.
+ *
+ * \ingroup systems
+ *
+ * \param registry the menu registry.
+ */
 void decrement_saves_button_group_page(entt::registry& registry);
 
 /// \} End of menus
