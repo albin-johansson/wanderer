42ed74ba3d008a99dd59e2e547a1be07faf649fc
Albin Johansson <albinoscar99@hotmail.com>
2021-04-24 15:41:31 +0200

    Update common_concepts.hpp

--- src/core/common_concepts.hpp
+++ src/core/common_concepts.hpp
@@ -10,6 +10,7 @@ namespace wanderer {
 
 // clang-format off
 
+/// Requires that a type features a `value_type` alias of a certain type.
 template <typename T, typename Elem>
 concept has_value_type = requires
 {
@@ -17,7 +18,7 @@ concept has_value_type = requires
   std::convertible_to<typename T::value_type, Elem>;
 };
 
-// An iterable container with a value type convertible to the specified element type
+/// An iterable container with a value type convertible to the specified element type
 template <typename T, typename Elem>
 concept container_type = std::ranges::range<T> &&
                          has_value_type<T, Elem>;
