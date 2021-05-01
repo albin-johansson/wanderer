7641c31474ed648ac209d4fa38a13060a7c59c46
Albin Johansson <albinoscar99@hotmail.com>
2021-05-01 22:05:04 +0200

    Update algorithms.hpp

--- src/core/algorithms.hpp
+++ src/core/algorithms.hpp
@@ -9,7 +9,7 @@ namespace wanderer {
 // clang-format off
 
 /// Returns a value accumulated from visiting each element in a range
-template <std::ranges::range T, std::invocable<typename T::value_type> Callable>
+template <std::ranges::range T, std::invocable<const typename T::value_type&> Callable>
 [[nodiscard]] constexpr auto accumulate(const T& range, Callable&& callable)
 {
   using value_type = std::invoke_result_t<Callable, typename T::value_type>;
