7b825e99022e9135205d851fa597b43c2720a2e1
Albin Johansson <albinoscar99@hotmail.com>
2021-05-04 00:06:48 +0200

    Update movable.hpp

--- src/components/movable.hpp
+++ src/components/movable.hpp
@@ -24,7 +24,7 @@ struct movable final
   float2 velocity;                 ///< Current x- and y-axis speed.
   float2 position;                 ///< Current position.
   direction dir{direction::down};  ///< Current dominant direction.
-  float speed{0};                  ///< Current maximum total speed of the movable.
+  float speed{};                   ///< Current maximum total speed of the movable.
 };
 
 void serialize(auto& archive, movable& m, u32 version)
