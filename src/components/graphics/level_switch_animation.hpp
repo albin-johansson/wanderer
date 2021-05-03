c08b7e361159356946cfff3974500de6944859e4
Albin Johansson <albinoscar99@hotmail.com>
2021-05-04 00:06:35 +0200

    Update level_switch_animation.hpp

--- src/components/graphics/level_switch_animation.hpp
+++ src/components/graphics/level_switch_animation.hpp
@@ -8,12 +8,12 @@ namespace wanderer::comp {
 struct level_switch_animation final
 {
   maybe<map_id> map;  ///< Optional ID of target map after animation.
-  float speed;        ///< Speed scale factor.
-  float width;        ///< Current width of the left and right "bars".
-  float height;       ///< Current height of the top- and bottom "bars".
-  float xStepSize;    ///< Size of an animation step in the x-axis.
-  float yStepSize;    ///< Size of an animation step in the y-axis.
-  bool fadingIn;      ///< Indicates whether or not the animation is fading in.
+  float speed{};      ///< Speed scale factor.
+  float width{};      ///< Current width of the left and right "bars".
+  float height{};     ///< Current height of the top- and bottom "bars".
+  float xStepSize{};  ///< Size of an animation step in the x-axis.
+  float yStepSize{};  ///< Size of an animation step in the y-axis.
+  bool fadingIn{};    ///< Indicates whether or not the animation is fading in.
 };
 
 }  // namespace wanderer::comp
