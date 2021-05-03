7f638ada13fcf499ef7050de101f1fd2e53f9ff1
Albin Johansson <albinoscar99@hotmail.com>
2021-05-04 00:06:42 +0200

    Update point_light.hpp

--- src/components/graphics/point_light.hpp
+++ src/components/graphics/point_light.hpp
@@ -13,11 +13,11 @@ namespace wanderer::comp {
  */
 struct point_light final
 {
-  float2 position;         ///< The world position of the light.
-  float size;              ///< Base size of the light.
-  float fluctuation;       ///< Fluctuation size in [-fluctuationLimit, fluctuationLimit].
-  float fluctuationStep;   ///< Size of each fluctuation increment.
-  float fluctuationLimit;  ///< Fluctuation size limit.
+  float2 position;      ///< The world position of the light.
+  float size{};         ///< Base size of the light.
+  float fluctuation{};  ///< Fluctuation size in [-fluctuationLimit, fluctuationLimit].
+  float fluctuationStep{};   ///< Size of each fluctuation increment.
+  float fluctuationLimit{};  ///< Fluctuation size limit.
 };
 
 void serialize(auto& archive, point_light& pl)
