0757bd3879c72ffecb1c84b55e2e29cea379ce4d
Albin Johansson <albinoscar99@hotmail.com>
2021-05-03 23:29:10 +0200

    Update Centurion to 6.0.0-beta

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 auto main(int, char**) -> int
 {
   cen::library centurion;
-  cen::log::set_priority(cen::log::priority::info);
+  cen::log::set_priority(cen::log_priority::info);
 
   wanderer::wanderer_engine engine;
   return engine.run();
