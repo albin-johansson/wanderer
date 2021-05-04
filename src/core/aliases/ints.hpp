0627532906cb974134203c43f077af1b5cacb1d6
Albin Johansson <albinoscar99@hotmail.com>
2021-05-04 13:00:32 +0200

    Update ints.hpp

--- src/core/aliases/ints.hpp
+++ src/core/aliases/ints.hpp
@@ -4,14 +4,17 @@
 
 namespace wanderer {
 
+using cen::uint;
+using cen::ulonglong;
+
+using cen::u8;
 using cen::u16;
 using cen::u32;
 using cen::u64;
-using cen::u8;
 
+using cen::i8;
 using cen::i16;
 using cen::i32;
 using cen::i64;
-using cen::i8;
 
 }  // namespace wanderer
