e3f4c24cd71c0112ddc106d2a9054dc572821f90
Albin Johansson <albinoscar99@hotmail.com>
2021-05-20 10:35:18 +0200

    Remove save_file_info.hpp

--- src/io/saves/parse_save_file.hpp
+++ src/io/saves/parse_save_file.hpp
@@ -1,11 +1,25 @@
 #pragma once
 
 #include <filesystem>  // path
+#include <vector>      // vector
 
 #include "core/aliases/map_id.hpp"
-#include "io/saves/save_file_info.hpp"
 
 namespace wanderer {
+
+struct save_file_level_info final
+{
+  map_id id;
+  std::filesystem::path path;
+  bool outside_level{};
+  bool keep_viewport_in_bounds{};
+};
+
+struct save_file_info final
+{
+  map_id current;
+  std::vector<save_file_level_info> levels;
+};
 
 [[nodiscard]] auto parse_save_file(const std::filesystem::path& path) -> save_file_info;
 
