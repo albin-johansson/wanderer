23f5de54aeb9b3f2d789eba0d0249b5fea2db41c
Albin Johansson <albinoscar99@hotmail.com>
2021-07-26 22:07:56 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,58 +6,14 @@
 
 #include <centurion.hpp>  // library
 #include <entt.hpp>       // id_type
-#include <format>         // format
-#include <iostream>       // clog
 #include <rune.hpp>       // ...
-#include <string_view>    // string_view
 
-#include "core/utils/time_utils.hpp"
 #include "wanderer_engine.hpp"
-
-namespace {
-
-[[nodiscard]] auto to_string(const SDL_LogPriority priority) noexcept -> std::string_view
-{
-  switch (priority)
-  {
-    default:
-      [[fallthrough]];
-
-    case SDL_LOG_PRIORITY_INFO:
-      return "INFO";
-
-    case SDL_LOG_PRIORITY_VERBOSE:
-      return "VERBOSE";
-
-    case SDL_LOG_PRIORITY_DEBUG:
-      return "DEBUG";
-
-    case SDL_LOG_PRIORITY_WARN:
-      return "WARNING";
-
-    case SDL_LOG_PRIORITY_ERROR:
-      return "ERROR";
-
-    case SDL_LOG_PRIORITY_CRITICAL:
-      return "CRITICAL";
-  }
-}
-
-void wanderer_log(void* /*data*/,
-                  const int /*category*/,
-                  const SDL_LogPriority priority,
-                  const char* msg)
-{
-  const auto time = wanderer::current_hhmmss();
-  std::clog << std::format("LOG {} [{}] > {}\n", time, to_string(priority), msg);
-}
-
-}  // namespace
 
 auto main(int, char**) -> int
 {
   cen::library centurion;
-  SDL_LogSetOutputFunction(wanderer_log, nullptr);
+  cen::log::use_preset_output_function();
 
   if constexpr (cen::is_debug_build())
   {
