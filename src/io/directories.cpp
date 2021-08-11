121968fd80c1bb6b39a8b79187465cb4da151abf
Albin Johansson <albinoscar99@hotmail.com>
2021-08-11 20:39:26 +0200

    Update directories.cpp

--- src/io/directories.cpp
+++ src/io/directories.cpp
@@ -6,14 +6,14 @@ namespace wanderer {
 
 auto files_directory() -> const std::filesystem::path&
 {
-  static std::filesystem::path path =
+  static const std::filesystem::path path =
       cen::preferred_path("albin-johansson", "wanderer").copy();
   return path;
 }
 
 auto saves_directory() -> const std::filesystem::path&
 {
-  static auto path = files_directory() / "saves";
+  static const auto path = files_directory() / "saves";
   return path;
 }
 
