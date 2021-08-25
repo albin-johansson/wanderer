c1d34bb39bdc39ebf79e3c6f1337caa518cf32d3
Albin Johansson <albinoscar99@hotmail.com>
2021-08-25 14:42:03 +0200

    Update test_main.cpp

--- test/unit/test_main.cpp
+++ test/unit/test_main.cpp
@@ -2,9 +2,15 @@
 
 #include <centurion.hpp>
 
+#include "io/rune_configuration.hpp"
+
 int main(int argc, char** argv)
 {
-  cen::library lib;
+  cen::library centurion;
+  cen::log::use_preset_output_function();
+
+  wanderer::prepare_rune_configuration();
+
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
