647d13d615b2540bcd39d87c972d15437a9caa1c
Albin Johansson <albinoscar99@hotmail.com>
2021-09-28 20:56:18 +0200

    Update time_of_day.hpp

--- src/components/ctx/time_of_day.hpp
+++ src/components/ctx/time_of_day.hpp
@@ -12,7 +12,7 @@ struct time_of_day final
   float hour{};     ///< [0, 24)
   float minute{};   ///< [0, 60)
   float seconds{};  ///< This has no predefined range, it is reset once per in-game day
-  int week{};       ///< The week index.
+  int32 week{};     ///< The week index.
   day_of_week day{day_of_week::monday};  ///< The current day.
   cen::color tint;
 };
