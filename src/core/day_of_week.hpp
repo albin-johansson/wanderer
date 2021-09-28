f035c2eecaeccf873dc50b14650a20df384e11fc
Albin Johansson <albinoscar99@hotmail.com>
2021-09-28 19:08:42 +0200

    Update day_of_week.hpp

--- src/core/day_of_week.hpp
+++ src/core/day_of_week.hpp
@@ -4,13 +4,13 @@ namespace wanderer {
 
 enum class day_of_week
 {
-  monday,
-  tuesday,
-  wednesday,
-  thursday,
-  friday,
-  saturday,
-  sunday
+  monday = 0,
+  tuesday = 1,
+  wednesday = 2,
+  thursday = 3,
+  friday = 4,
+  saturday = 5,
+  sunday = 6
 };
 
 }  // namespace wanderer
