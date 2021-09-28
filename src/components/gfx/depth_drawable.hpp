f90de0fb294d7bf2d1c010ee328127201a8f9f5b
Albin Johansson <albinoscar99@hotmail.com>
2021-09-28 20:40:48 +0200

    Update depth_drawable.hpp

--- src/components/gfx/depth_drawable.hpp
+++ src/components/gfx/depth_drawable.hpp
@@ -32,7 +32,7 @@ struct depth_drawable final
   texture_index texture{};  ///< ID of associated texture.
   cen::irect src;           ///< Region of associated texture that will be rendered.
   cen::frect dst;           ///< Position and size of the rendered texture, in pixels.
-  int layer{};              ///< Layer index.
+  int32 layer{};            ///< Layer index.
   depth_t depth{5};         ///< Render order heuristic.
 };
 
