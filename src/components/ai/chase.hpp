c48116ddbe5f227aee44d55aaadb3d028f4616c5
Albin Johansson <albinoscar99@hotmail.com>
2021-09-29 19:48:34 +0200

    Update chase.hpp

--- src/components/ai/chase.hpp
+++ src/components/ai/chase.hpp
@@ -2,27 +2,13 @@
 
 #include <entt.hpp>  // entity, null
 
-#include "common/entity_type.hpp"
-#include "common/ints.hpp"
-
 namespace wanderer::comp {
-
-namespace tags {
-struct chase_tag;
-}  // namespace tags
 
 struct chase final
 {
-  using entity = entity_type<tags::chase_tag>;
-
   entt::entity target{entt::null};  ///< The entity that will be chased.
   float range{};  ///< The maximum distance at which the target can be chased.
   bool active{};  ///< Whether or not the target is within range.
 };
-
-void serialize(auto& archive, chase& c, uint32 version)
-{
-  archive(c.target, c.range, c.active);
-}
 
 }  // namespace wanderer::comp
