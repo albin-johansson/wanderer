ed8249fc8ec06d33ae436cc3f6a047e0714815a1
Albin Johansson <albinoscar99@hotmail.com>
2021-09-29 19:49:55 +0200

    Update humanoid.hpp

--- src/components/ai/humanoid.hpp
+++ src/components/ai/humanoid.hpp
@@ -1,85 +1,30 @@
 #pragma once
 
-#include <entt.hpp>  // entity
-
-#include "common/ints.hpp"
-#include "core/direction.hpp"
+#include <entt.hpp>  // entity, null
 
 namespace wanderer::comp {
 
-/**
- * \struct humanoid
- *
- * \brief A tag type used to identify humanoids.
- *
- * \headerfile humanoid_state.hpp
- */
+/// A tag type used to identify humanoids.
 struct humanoid final
 {};
 
-/**
- * \struct humanoid_idle
- *
- * \brief A tag type used to identify the idle state of a humanoid.
- *
- * \headerfile humanoid_state.hpp
- */
+/// A tag type used to identify the idle state of a humanoid.
 struct humanoid_idle final
 {};
 
-/**
- * \struct humanoid_move
- *
- * \brief A tag type used to identify the moving state of a humanoid.
- *
- * \headerfile humanoid_state.hpp
- */
+/// A tag type used to identify the moving state of a humanoid.
 struct humanoid_move final
 {};
 
-/**
- * \struct humanoid_attack
- *
- * \brief Represents the attack state of a humanoid.
- *
- * \var humanoid_attack::weapon
- * The weapon used for the attack.
- * \var humanoid_attack::done
- * Indicates whether or not the attack is done.
- *
- * \headerfile humanoid_state.hpp
- */
+/// Represents the attack state of a humanoid.
 struct humanoid_attack final
 {
-  entt::entity weapon{entt::null};  // TODO weapon component entity tag
-  bool done{};
+  entt::entity weapon{entt::null};  ///< The weapon used for the attack.
+  bool done{};                      ///< Indicates whether or not the attack is done.
 };
 
-/**
- * \struct humanoid_die
- *
- * \brief A tag type used to identify the dying state of a humanoid.
- *
- * \headerfile humanoid_state.hpp
- */
+/// A tag type used to identify the dying state of a humanoid.
 struct humanoid_die final
 {};
-
-void serialize(auto&, humanoid&, uint32 version)
-{}
-
-void serialize(auto&, humanoid_idle&, uint32 version)
-{}
-
-void serialize(auto&, humanoid_move&, uint32 version)
-{}
-
-void serialize(auto& archive, humanoid_attack& ha, uint32 version)
-{
-  archive(ha.weapon, ha.done);
-}
-
-void serialize(auto&, humanoid_die&, uint32 version)
-{}
 
 }  // namespace wanderer::comp
