88e9c9a5fd38cf0eeae25071569644c949570317
Albin Johansson <albinoscar99@hotmail.com>
2021-10-01 19:44:03 +0200

    Update humanoid.hpp

--- src/components/ai/humanoid.hpp
+++ src/components/ai/humanoid.hpp
@@ -4,27 +4,32 @@
 
 namespace wanderer::comp {
 
-/// A tag type used to identify humanoids.
+/// \addtogroup components
+/// \{
+
+/// \brief A tag type used to identify humanoids.
 struct humanoid final
 {};
 
-/// A tag type used to identify the idle state of a humanoid.
+/// \brief A tag type used to identify the idle state of a humanoid.
 struct humanoid_idle final
 {};
 
-/// A tag type used to identify the moving state of a humanoid.
+/// \brief A tag type used to identify the moving state of a humanoid.
 struct humanoid_move final
 {};
 
-/// Represents the attack state of a humanoid.
+/// \brief Represents the attack state of a humanoid.
 struct humanoid_attack final
 {
   entt::entity weapon{entt::null};  ///< The weapon used for the attack.
   bool done{};                      ///< Indicates whether or not the attack is done.
 };
 
-/// A tag type used to identify the dying state of a humanoid.
+/// \brief A tag type used to identify the dying state of a humanoid.
 struct humanoid_die final
 {};
+
+/// \} End of group components
 
 }  // namespace wanderer::comp
