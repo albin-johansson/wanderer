5a98e7824d3203947db3793bdafbca08e8b9a483
Albin Johansson <albinoscar99@hotmail.com>
2021-10-01 19:47:48 +0200

    Update point_light.hpp

--- src/components/gfx/point_light.hpp
+++ src/components/gfx/point_light.hpp
@@ -4,18 +4,13 @@
 
 namespace wanderer::comp {
 
-/**
- * \struct point_light
- *
- * \brief Represents a light source.
- *
- * \ingroup components
- */
+/// \brief Represents light sources focused at specific points.
+/// \ingroup components
 struct point_light final
 {
   float2 position;      ///< The world position of the light.
   float size{};         ///< Base size of the light.
-  float fluctuation{};  ///< Fluctuation size in [-fluctuationLimit, fluctuationLimit].
+  float fluctuation{};  ///< Fluctuation size in [-fluctuation_limit, fluctuation_limit].
   float fluctuation_step{};   ///< Size of each fluctuation increment.
   float fluctuation_limit{};  ///< Fluctuation size limit.
 };
