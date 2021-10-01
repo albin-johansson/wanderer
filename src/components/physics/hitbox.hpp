874653fad2040e2cd4b346ccc21bd379259f8c24
Albin Johansson <albinoscar99@hotmail.com>
2021-10-01 20:09:51 +0200

    Update hitbox.hpp

--- src/components/physics/hitbox.hpp
+++ src/components/physics/hitbox.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <array>                // array
 #include <centurion.hpp>        // farea, frect
 #include <rune/everything.hpp>  // static_vector
 
@@ -12,30 +11,24 @@ namespace wanderer::comp {
 /// The maximum number of subhitboxes a single hitbox can contain.
 inline constexpr usize max_subhitbox_count = 5;
 
-/**
- * \brief Represents a "subhitbox", a member of a `hitbox` instance.
- *
- * \headerfile hitbox.hpp
- */
+/// \brief Represents a "subhitbox", a member of a `hitbox` instance.
+/// \ingroup components
 struct subhitbox final
 {
   float2 offset;    ///< Offset relative to the parent hitbox.
   cen::farea size;  ///< The size of the subhitbox.
 };
 
-/**
- * \brief Represents a hitbox that supports multiple subhitboxes.
- *
- * \ingroup components
- *
- * \headerfile hitbox.hpp
- */
+/// \brief Represents a hitbox that supports multiple subhitboxes.
+/// \ingroup components
 struct hitbox final
 {
-  float2 origin;      ///< Position of the hitbox
-  cen::frect bounds;  ///< Bounding rectangle of all subhitboxes
-  rune::static_vector<subhitbox, max_subhitbox_count> boxes;  ///< Associated subhitboxes
-  bool enabled{true};  ///< Whether or not the hitbox can collide with other hitboxes
+  using hitbox_vector = rune::static_vector<subhitbox, max_subhitbox_count>;
+
+  float2 origin;        ///< Position of the hitbox.
+  cen::frect bounds;    ///< Bounding rectangle of all subhitboxes.
+  hitbox_vector boxes;  ///< Associated subhitboxes.
+  bool enabled{true};   ///< Whether or not the hitbox can collide with other hitboxes.
 };
 
 }  // namespace wanderer::comp
