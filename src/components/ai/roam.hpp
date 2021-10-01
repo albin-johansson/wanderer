e80bf19837dfb94b0865858e1d192862fee7da2b
Albin Johansson <albinoscar99@hotmail.com>
2021-10-01 19:43:56 +0200

    Update roam.hpp

--- src/components/ai/roam.hpp
+++ src/components/ai/roam.hpp
@@ -5,11 +5,13 @@
 
 namespace wanderer::comp {
 
+/// \brief A component used by NPC entities that can roam randomly.
+/// \ingroup components
 struct roam final
 {
-  maybe<float2> destination;
-  float cooldown{};
-  float cooldown_duration{};
+  maybe<float2> destination;  ///< Target position to roam to.
+  float cooldown{};           ///< The current roam cooldown progress.
+  float cooldown_duration{};  ///< The total duration of the cooldown.
 };
 
 }  // namespace wanderer::comp
