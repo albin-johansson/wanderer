782306265855d5674ba50089afbd33f59fa1cc31
Albin Johansson <albinoscar99@hotmail.com>
2022-01-21 22:02:15 +0100

    Update profile.hpp

---
+++ source/profile.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <centurion.hpp>
+
+#ifdef NDEBUG
+#define WANDERER_PROFILE_START
+#else
+#define WANDERER_PROFILE_START const auto wndr_profile_start = cen::now();
+#endif  // NDEBUG
+
+#ifdef NDEBUG
+#define WANDERER_PROFILE_END(Msg)
+#else
+#define WANDERER_PROFILE_END(Msg)                                       \
+  const auto wndr_profile_end = cen::now();                             \
+  const auto wndr_profile_diff =                                        \
+      static_cast<double>(wndr_profile_end - wndr_profile_start);       \
+  const auto wndr_profile_freq = static_cast<double>(cen::frequency()); \
+  CENTURION_LOG_DEBUG("[PROFILE] " Msg ": %f seconds",                  \
+                      wndr_profile_diff / wndr_profile_freq);
+#endif  // NDEBUG
