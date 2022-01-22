782e6a7e67a81287da250d73b51cf0ca0a06c8c9
Albin Johansson <albinoscar99@hotmail.com>
2022-01-22 02:57:09 +0100

    Add logging utilities

--- source/logging.cpp
+++ source/logging.cpp
@@ -1,1 +1,46 @@
 #include "logging.hpp"
+
+#include <ctime>  // time
+
+#include <fmt/chrono.h>
+
+namespace wanderer::logging {
+namespace {
+
+void log_impl(const fmt::color color,
+              const std::string_view category,
+              const std::string_view fmt,
+              const fmt::format_args args)
+{
+  const auto msg = fmt::vformat(fmt, args);
+
+  const auto time = fmt::localtime(std::time(nullptr));
+  const auto full = fmt::vformat("{:>9} {:%H:%M:%S} > {}\n",
+                                 fmt::make_format_args(category, time, msg));
+
+  fmt::print(fmt::fg(color), "{}", full);
+}
+
+}  // namespace
+
+void log_debug(const std::string_view fmt, const fmt::format_args args)
+{
+  log_impl(fmt::color::aquamarine, "[debug]", fmt, args);
+}
+
+void log_info(const std::string_view fmt, const fmt::format_args args)
+{
+  log_impl(fmt::color::floral_white, "[info]", fmt, args);
+}
+
+void log_warn(const std::string_view fmt, const fmt::format_args args)
+{
+  log_impl(fmt::color::yellow, "[warn]", fmt, args);
+}
+
+void log_error(const std::string_view fmt, const fmt::format_args args)
+{
+  log_impl(fmt::color::orange_red, "[error]", fmt, args);
+}
+
+}  // namespace wanderer::logging
--- source/logging.hpp
+++ source/logging.hpp
@@ -1,7 +1,57 @@
 #pragma once
 
+#include <string_view>  // string_view
+
+#include <fmt/color.h>
+#include <fmt/core.h>
+
+#include "build.hpp"
+
 namespace wanderer {
+namespace logging {
 
+void log_debug(std::string_view fmt, fmt::format_args args);
+void log_info(std::string_view fmt, fmt::format_args args);
+void log_warn(std::string_view fmt, fmt::format_args args);
+void log_error(std::string_view fmt, fmt::format_args args);
 
+}  // namespace logging
 
+template <typename... Args>
+void print([[maybe_unused]] const fmt::color color,
+           const std::string_view fmt,
+           const Args&... args)
+{
+  if constexpr (is_windows) {
+    fmt::print(fmt, args...);
+  }
+  else {
+    fmt::print(fmt::fg(color), fmt, args...);
+  }
 }
+
+template <typename... Args>
+void log_debug(const std::string_view fmt, const Args&... args)
+{
+  logging::log_debug(fmt, fmt::make_format_args(args...));
+}
+
+template <typename... Args>
+void log_info(const std::string_view fmt, const Args&... args)
+{
+  logging::log_info(fmt, fmt::make_format_args(args...));
+}
+
+template <typename... Args>
+void log_warn(const std::string_view fmt, const Args&... args)
+{
+  logging::log_warn(fmt, fmt::make_format_args(args...));
+}
+
+template <typename... Args>
+void log_error(const std::string_view fmt, const Args&... args)
+{
+  logging::log_error(fmt, fmt::make_format_args(args...));
+}
+
+}  // namespace wanderer
