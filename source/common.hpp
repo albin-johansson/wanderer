df9f97fe33eaa4ef7ccd17c099567a0468d4a540
Albin Johansson <albinoscar99@hotmail.com>
2022-01-22 02:32:12 +0100

    Create common.hpp

---
+++ source/common.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <cstddef>   // size_t
+#include <cstdint>   // int{}_t, uint{}_t
+#include <optional>  // optional, nullopt_t, nullopt
+
+#include <glm/glm.hpp>
+
+namespace wanderer {
+
+using usize = std::size_t;
+
+using uchar = unsigned char;
+using uint = unsigned int;
+using ulonglong = unsigned long long int;
+
+using int8 = std::int8_t;
+using int16 = std::int16_t;
+using int32 = std::int32_t;
+using int64 = std::int64_t;
+
+using uint8 = std::uint8_t;
+using uint16 = std::uint16_t;
+using uint32 = std::uint32_t;
+using uint64 = std::uint64_t;
+
+using float32 = float;
+using float64 = double;
+
+static_assert(sizeof(float32) == 4);
+static_assert(sizeof(float64) == 8);
+
+template <typename T>
+using maybe = std::optional<T>;
+
+inline constexpr std::nullopt_t nothing = std::nullopt;
+
+}  // namespace wanderer
