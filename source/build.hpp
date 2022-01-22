080d8a89504ba24ede012db58d3829958c9bc9dd
Albin Johansson <albinoscar99@hotmail.com>
2022-01-22 02:55:30 +0100

    Update build.hpp

--- source/build.hpp
+++ source/build.hpp
@@ -22,4 +22,16 @@ inline constexpr bool is_debug_build = false;
 inline constexpr bool is_release_build = !is_debug_build;
 #endif  // WANDERER_DEBUG_BUILD
 
+#ifdef WIN32
+inline constexpr bool is_windows = true;
+#else
+inline constexpr bool is_windows = false;
+#endif  // WIN32
+
+#ifdef __APPLE__
+inline constexpr bool is_osx = true;
+#else
+inline constexpr bool is_osx = false;
+#endif  // __APPLE__
+
 }  // namespace wanderer
