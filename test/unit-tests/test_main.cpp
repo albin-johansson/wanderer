5930a63c0144711c57746289853e467b8286f17f
Albin Johansson <albinoscar99@hotmail.com>
2022-02-06 23:42:27 +0100

    Use doctest instead of gtest

--- test/unit-tests/test_main.cpp
+++ test/unit-tests/test_main.cpp
@@ -1,13 +1,21 @@
-#include <centurion.hpp>
-#include <gtest/gtest.h>
+#define DOCTEST_CONFIG_IMPLEMENT
 
-int main(int argc, char** argv)
+#include <centurion.hpp>
+#include <doctest/doctest.h>
+
+auto main(int, char**) -> int
 {
   const cen::sdl sdl;
   const cen::img img;
   const cen::mix mix;
   const cen::ttf ttf;
 
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  doctest::Context context;
+  const auto result = context.run();
+
+  if (context.shouldExit()) {
+    return result;
+  }
+
+  return result;
 }
