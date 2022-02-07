ade9a092d2545c71e2f17dfce25cebeb5fc105c4
Albin Johansson <albinoscar99@hotmail.com>
2022-02-07 11:54:09 +0100

    Update common.hpp

--- source/wanderer/common.hpp
+++ source/wanderer/common.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
-#include <cstddef>   // size_t
-#include <cstdint>   // int{}_t, uint{}_t
-#include <optional>  // optional, nullopt_t, nullopt
+#include <cstddef>        // size_t
+#include <cstdint>        // int{}_t, uint{}_t
+#include <optional>       // optional, nullopt_t, nullopt
+#include <unordered_map>  // unordered_map
+#include <map>            // map
+#include <functional>     // less
 
 #include <glm/glm.hpp>
 
@@ -30,9 +33,20 @@ using float64 = double;
 static_assert(sizeof(float32) == 4);
 static_assert(sizeof(float64) == 8);
 
+template <typename K, typename V>
+using tree_map = std::map<K, V, std::less<>>;
+
+template <typename K, typename V>
+using hash_map = std::unordered_map<K, V>;
+
 template <typename T>
 using maybe = std::optional<T>;
 
 inline constexpr std::nullopt_t nothing = std::nullopt;
+
+using texture_id = uint32;
+using tile_id = uint32;
+
+inline constexpr tile_id empty_tile = 0;
 
 }  // namespace wanderer
