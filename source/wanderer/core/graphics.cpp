452f3d6524a50bec94833fa19fd9b40fafe6e0ca
Albin Johansson <albinoscar99@hotmail.com>
2022-02-14 10:31:44 +0100

    Add graphics_context class

---
+++ source/wanderer/core/graphics.cpp
@@ -0,0 +1,90 @@
+#include "graphics.hpp"
+
+#include "wanderer/data/cfg.hpp"
+#include "wanderer/misc/exception.hpp"
+
+namespace wanderer {
+namespace {
+
+constexpr auto _window_flags = cen::window::hidden | cen::window::allow_high_dpi;
+
+constexpr auto _renderer_flags = cen::renderer::accelerated |      //
+                                 cen::renderer::target_textures |  //
+                                 cen::renderer::vsync;
+
+constexpr int _pixelated_size_s = 8;
+constexpr int _pixelated_size_m = _pixelated_size_s * 2;
+constexpr int _pixelated_size_l = _pixelated_size_s * 3;
+
+constexpr int _handwriting_size_s = 9;
+constexpr int _handwriting_size_m = 12;
+constexpr int _handwriting_size_l = 16;
+
+}  // namespace
+
+graphics_ctx::graphics_ctx(const game_cfg& cfg)
+    : mWindow{"Wanderer", cen::window::default_size(), _window_flags}
+    , mRenderer{mWindow.create_renderer(_renderer_flags)}
+{
+  const cen::iarea size{cfg.logical_size.x, cfg.logical_size.y};
+  mWindow.set_size(size);
+
+  mRenderer.set_logical_size(size);
+  mRenderer.set_blend_mode(cen::blend_mode::blend);
+
+  const char* pixelatedPath = "resources/fonts/type_writer.ttf";
+  mPixelatedFontId = mFontBundle.load_font(pixelatedPath, _pixelated_size_s);
+  mFontBundle.load_font(pixelatedPath, _pixelated_size_m);
+  mFontBundle.load_font(pixelatedPath, _pixelated_size_l);
+
+  const char* handwritingPath = "resources/fonts/daniel.ttf";
+  mHandwritingFontId = mFontBundle.load_font(handwritingPath, _handwriting_size_s);
+  mFontBundle.load_font(handwritingPath, _handwriting_size_m);
+  mFontBundle.load_font(handwritingPath, _handwriting_size_l);
+}
+
+auto graphics_ctx::get_pixelated_font(const font_size size) -> cen::font&
+{
+  switch (size) {
+    case font_size::small:
+      return mFontBundle.at(mPixelatedFontId, _pixelated_size_s).get_font();
+
+    case font_size::medium:
+      return mFontBundle.at(mPixelatedFontId, _pixelated_size_m).get_font();
+
+    case font_size::large:
+      return mFontBundle.at(mPixelatedFontId, _pixelated_size_l).get_font();
+
+    default:
+      throw_traced(wanderer_error{"Invalid font size!"});
+  }
+}
+
+auto graphics_ctx::get_handwriting_font(const font_size size) -> cen::font&
+{
+  switch (size) {
+    case font_size::small:
+      return mFontBundle.at(mHandwritingFontId, _handwriting_size_s).get_font();
+
+    case font_size::medium:
+      return mFontBundle.at(mHandwritingFontId, _handwriting_size_m).get_font();
+
+    case font_size::large:
+      return mFontBundle.at(mHandwritingFontId, _handwriting_size_l).get_font();
+
+    default:
+      throw_traced(wanderer_error{"Invalid font size!"});
+  }
+}
+
+auto graphics_ctx::window() -> cen::window&
+{
+  return mWindow;
+}
+
+auto graphics_ctx::renderer() -> cen::renderer&
+{
+  return mRenderer;
+}
+
+}  // namespace wanderer
---
+++ source/wanderer/core/graphics.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <centurion.hpp>
+
+#include "wanderer/common.hpp"
+#include "wanderer/fwd.hpp"
+
+namespace wanderer {
+
+enum class font_id
+{
+  daniel,
+  type_writer
+};
+
+enum class font_size
+{
+  small,
+  medium,
+  large
+};
+
+class graphics_ctx final
+{
+ public:
+  explicit graphics_ctx(const game_cfg& cfg);
+
+  [[nodiscard]] auto get_pixelated_font(font_size size) -> cen::font&;
+
+  [[nodiscard]] auto get_handwriting_font(font_size size) -> cen::font&;
+
+  [[nodiscard]] auto window() -> cen::window&;
+
+  [[nodiscard]] auto renderer() -> cen::renderer&;
+
+ private:
+  cen::window mWindow;
+  cen::renderer mRenderer;
+
+  cen::font_bundle mFontBundle;
+  cen::font_bundle::id_type mPixelatedFontId;
+  cen::font_bundle::id_type mHandwritingFontId;
+};
+
+}  // namespace wanderer
