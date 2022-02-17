dc76c7b07259c6af98a234d3ccd43fa8c8c712aa
Albin Johansson <albinoscar99@hotmail.com>
2022-02-17 10:59:00 +0100

    Tweak registry system

--- source/wanderer/systems/registry_system.cpp
+++ source/wanderer/systems/registry_system.cpp
@@ -1,6 +1,9 @@
 #include "registry_system.hpp"
 
 #include "wanderer/data/components/tiles.hpp"
+#include "wanderer/data/components/levels.hpp"
+#include "wanderer/data/components/rendering.hpp"
+#include "wanderer/data/cfg.hpp"
 
 namespace wanderer::sys {
 
@@ -8,15 +11,21 @@ auto make_main_registry() -> entt::registry
 {
   entt::registry registry;
 
+  registry.set<comp::level_ctx>();
+
   return registry;
 }
 
-auto make_level_registry() -> entt::registry
+auto make_level_registry(const game_cfg& cfg) -> entt::registry
 {
   entt::registry registry;
 
   registry.set<comp::tilemap>();
   registry.set<comp::tilesets>();
+  registry.set<comp::render_bounds>();
+
+  auto& viewport = registry.set<comp::viewport>();
+  viewport.size = cfg.logical_size_f;
 
   return registry;
 }
--- source/wanderer/systems/registry_system.hpp
+++ source/wanderer/systems/registry_system.hpp
@@ -2,10 +2,15 @@
 
 #include <entt/entt.hpp>
 
+#include "wanderer/fwd.hpp"
+
 namespace wanderer::sys {
 
 /**
  * \brief Creates a main registry, used as the "top-level" registry for the game.
+ *
+ * \details The main registry has the following context components:
+ * - `comp::level_ctx`
  *
  * \return a registry.
  */
@@ -14,8 +19,16 @@ namespace wanderer::sys {
 /**
  * \brief Creates a registry for use by individual levels.
  *
+ * \details A level registry has the following context components:
+ * - `comp::tilemap`
+ * - `comp::tilesets`
+ * - `comp::render_bounds`
+ * - `comp::viewport`
+ *
+ * \param cfg the current game configuration.
+ *
  * \return a registry.
  */
-[[nodiscard]] auto make_level_registry() -> entt::registry;
+[[nodiscard]] auto make_level_registry(const game_cfg& cfg) -> entt::registry;
 
 }  // namespace wanderer::sys
