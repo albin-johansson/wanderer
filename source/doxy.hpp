5ef697cfd865b634c71972b27b7e9fb5bfff4123
Albin Johansson <albinoscar99@hotmail.com>
2022-02-20 22:57:51 +0100

    Create doxy.hpp

---
+++ source/doxy.hpp
@@ -0,0 +1,29 @@
+/**
+ * \defgroup core
+ */
+
+/**
+ * \defgroup data
+ */
+
+/**
+ * \ingroup data
+ * \defgroup components
+ */
+
+/**
+ * \defgroup events
+ */
+
+/**
+ * \defgroup io I/O
+ */
+
+/**
+ * \defgroup misc Miscellaneous
+ */
+
+/**
+ * \defgroup systems
+ */
+
