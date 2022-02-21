2009048f79e29d14d80d4410dce197cd1f6b8e41
Albin Johansson <albinoscar99@hotmail.com>
2022-02-21 17:42:50 +0100

    Update build.hpp

--- source/wanderer/meta/build.hpp
+++ source/wanderer/meta/build.hpp
@@ -12,6 +12,18 @@
 
 #endif  // NDEBUG
 
+#ifdef WIN32
+#define WANDERER_PLATFORM_WINDOWS 1
+#else
+#define WANDERER_PLATFORM_WINDOWS 0
+#endif  // WIN32
+
+#ifdef __APPLE__
+#define WANDERER_PLATFORM_OSX 1
+#else
+#define WANDERER_PLATFORM_OSX 0
+#endif  // __APPLE__
+
 #ifdef _MSC_VER
 #define WANDERER_COMPILER_MSVC 1
 #else
@@ -40,16 +52,20 @@ inline constexpr bool is_debug_build = false;
 inline constexpr bool is_release_build = !is_debug_build;
 #endif  // WANDERER_DEBUG_BUILD
 
-#ifdef WIN32
+#if WANDERER_PLATFORM_WINDOWS
 inline constexpr bool is_windows = true;
+inline constexpr bool on_windows = true;
 #else
 inline constexpr bool is_windows = false;
-#endif  // WIN32
+inline constexpr bool on_windows = false;
+#endif  // WANDERER_PLATFORM_WINDOWS
 
-#ifdef __APPLE__
+#ifdef WANDERER_PLATFORM_OSX
 inline constexpr bool is_osx = true;
+inline constexpr bool on_osx = true;
 #else
 inline constexpr bool is_osx = false;
-#endif  // __APPLE__
+inline constexpr bool on_osx = false;
+#endif  // WANDERER_PLATFORM_OSX
 
 }  // namespace wanderer
