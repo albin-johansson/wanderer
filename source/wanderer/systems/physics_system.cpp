7cb637537e44803cfdaaede5ac6a2f38a07ab6cd
Albin Johansson <albinoscar99@hotmail.com>
2022-02-23 23:44:31 +0100

    Update physics_system.cpp

--- source/wanderer/systems/physics_system.cpp
+++ source/wanderer/systems/physics_system.cpp
@@ -1,5 +1,6 @@
 #include "physics_system.hpp"
 
+#include "wanderer/core/centurion_utils.hpp"
 #include "wanderer/core/graphics.hpp"
 #include "wanderer/core/math.hpp"
 #include "wanderer/data/cfg.hpp"
@@ -80,7 +81,9 @@ void update_physics(entt::registry& registry, const float32 dt)
 void debug_physics(const entt::registry& registry, graphics_ctx& graphics)
 {
   const auto& world = registry.ctx<comp::physics_world>();
+
   const auto& viewport = registry.ctx<comp::viewport>();
+  const auto viewportRect = as_rect(viewport.size, viewport.size);
 
   auto& renderer = graphics.renderer();
   renderer.set_color(cen::colors::magenta);
@@ -89,11 +92,13 @@ void debug_physics(const entt::registry& registry, graphics_ctx& graphics)
     const auto position = to_logical_world(registry, body.data->GetPosition());
     const auto size = to_logical_world(registry, body.size);
 
-    const cen::frect hitbox{position.x - viewport.offset.x,
-                            position.y - viewport.offset.y,
-                            size.x,
-                            size.y};
-    renderer.draw_rect(hitbox);
+    cen::frect hitbox{position.x, position.y, size.x, size.y};
+    if (cen::intersects(viewportRect, hitbox)) {
+      hitbox.offset_x(-viewport.offset.x);
+      hitbox.offset_y(-viewport.offset.y);
+
+      renderer.draw_rect(hitbox);
+    }
   }
 }
 
