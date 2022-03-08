a9376ab18cdbdd5127efe668f27f3a08c0af3dde
Albin Johansson <albinoscar99@hotmail.com>
2022-03-08 19:10:34 +0100

    Update animation_system.cpp

--- source/wanderer/systems/animation_system.cpp
+++ source/wanderer/systems/animation_system.cpp
@@ -9,11 +9,22 @@ void update_animations(entt::registry& registry)
   for (auto&& [entity, animation] : registry.view<comp::animation>().each()) {
     const auto now = cen::ticks64();
     const auto elapsed = now - animation.then;
-    if (elapsed >= animation.delays.at(animation.frame)) {
+
+    const auto frameDuration = animation.delays.size() == 1
+                                   ? animation.delays.front()
+                                   : animation.delays.at(animation.frame);
+
+    if (elapsed >= frameDuration) {
       animation.then = now;
       animation.frame = (animation.frame + 1u) % animation.frame_count;
     }
   }
+
+  for (auto&& [entity, animation, seq, drawable] :
+       registry.view<comp::animation, comp::seq_animation, comp::drawable>().each()) {
+    drawable.src.set_x(static_cast<int32>(animation.frame) * seq.frame_size.x);
+  }
+}
 }
 
 }  // namespace wanderer::sys
