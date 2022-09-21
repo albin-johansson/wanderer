6dc878860179f27c5e54a6930c038af4ac6802a7
Albin Johansson <albinoscar99@hotmail.com>
2022-09-21 22:00:08 +0200

    Switch to using gtest

--- test/src/test_main.cpp
+++ test/src/test_main.cpp
@@ -1,21 +1,13 @@
-#define DOCTEST_CONFIG_IMPLEMENT
+#include <centurion/initialization.hpp>
+#include <gtest/gtest.h>
 
-#include <centurion.hpp>
-#include <doctest/doctest.h>
-
-auto main(int, char**) -> int
+auto main(int argc, char* argv[]) -> int
 {
   const cen::sdl sdl;
   const cen::img img;
   const cen::mix mix;
   const cen::ttf ttf;
 
-  doctest::Context context;
-  const auto result = context.run();
-
-  if (context.shouldExit()) {
-    return result;
-  }
-
-  return result;
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
