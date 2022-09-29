6195ba6c54815a80c8703b278b60e7538ee51c56
Albin Johansson <albinoscar99@hotmail.com>
2022-09-29 20:37:37 +0200

    Add OpenGL stuff

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include <google/protobuf/stubs/common.h>
 #include <spdlog/spdlog.h>
 
+#include "wanderer/core/game.hpp"
+
 using wanderer::Game;
 
 namespace {
@@ -17,22 +19,38 @@ struct ProtobufContext final {
 
 }  // namespace
 
-int main(int, char*[])
+auto main(int, char*[]) -> int
 {
   std::set_terminate([] { std::abort(); });  // Avoid redundant logging
 
   try {
-    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
-
     spdlog::set_level(spdlog::level::debug);
     spdlog::info("Booting...");
 
     ProtobufContext protobuf {};
 
     cen::sdl sdl;
-    cen::img sdl_img;
     cen::ttf sdl_ttf;
     cen::mix sdl_mix;
+
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
+    SDL_SetHint(SDL_HINT_RENDER_LOGICAL_SIZE_MODE, "letterbox");
+    SDL_SetHint(SDL_HINT_RENDER_BATCHING, "1");
+    SDL_SetHint(SDL_HINT_RENDER_LINE_METHOD, "1");
+    SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengl");
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+#ifdef __APPLE__
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+#endif  // __APPLE__
+
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    Game game;
+    game.run();
 
     return EXIT_SUCCESS;
   }
